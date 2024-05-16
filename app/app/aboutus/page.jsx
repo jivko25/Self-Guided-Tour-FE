a315175500b20f6350cbca3f6e012f1b4524020b
ajummer <makeudreamempty@gmail.com>
2024-05-16 18:41:09 +0300

    feature: setup nextjs file structure & clear default code

---
+++ app/app/aboutus/page.jsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return (
+    <main>
+      <div>About us</div>
+    </main>
+  );
+}
---
+++ app/app/page.jsx
@@ -0,0 +1,11 @@
+import variables from "./styles/_variables.module.scss"
+
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className={variables.title}>Home</h1>
+   </main>
+  )
+}
+
