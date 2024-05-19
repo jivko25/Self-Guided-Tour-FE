df141ab484949c487e0f18b5d2dea7652161db0f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-19 16:13:51 +0300

    Update layout.jsx

--- app/app/layout.jsx
+++ app/app/layout.jsx
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <AppRouterCacheProvider>
-         
           <Header />
           {children}
           <Footer />
