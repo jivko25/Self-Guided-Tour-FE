dd7ed58e83d7117f5d759e612d6009fb3853194b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-22 13:22:32 +0300

    Create requester.jsx

---
+++ app/app/lib/requester.jsx
@@ -0,0 +1,44 @@
+const buildOptions = (data) => {
+    const options = {};
+  
+    if (data) {
+      options.body = JSON.stringify(data);
+      options.headers = {
+        "content-type": "application/json",
+      };
+    }
+  
+    const token = localStorage.getItem("access_token");
+  
+    if (token) {
+      options.headers = {
+        ...(options.headers = {
+          "X-Authorization": token,
+        }),
+      };
+    }
+    return options;
+  };
+  
+  const request = async (method, url, data) => {
+    const response = await fetch(url, {
+      ...buildOptions(data),
+      method,
+    });
+  
+    if (response.status === 204) {
+      return {};
+    }
+    const result = await response.json();
+  
+    if (!response.ok) {
+      throw result;
+    }
+    return result;
+  };
+  
+  export const get = request.bind(null, "GET")
+  export const post = request.bind(null, "POST")
+  export const put = request.bind(null, "PUT")
+  export const del = request.bind(null, "DELETE")
+  export const patch = request.bind(null, "PATCH")
