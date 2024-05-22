cde9251366e2f0aca422d464b9fa27526891ff34
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-22 13:22:36 +0300

    Create authService.jsx

---
+++ app/app/services/authService.jsx
@@ -0,0 +1,19 @@
+import * as request from "../lib/requester"
+
+const baseUrl = 'http://localhost:3000/auth'
+
+export const login = async (email, password) => {
+    const result = await request.post(`${baseUrl}/login`, {
+        email,
+        password
+    })
+}
+
+export const register = async (email, password) => {
+    const result = await request.post(`${baseUrl}/register`, {
+        email, password
+    })
+    return result
+}
+
+export const logout = () => request.get(`${baseUrl}/logout`)
