ef15619694894951953ffcc454a327c2fc7df367
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-22 14:11:10 +0300

    Update usePersistedState.jsx

--- app/app/hooks/usePersistedState.jsx
+++ app/app/hooks/usePersistedState.jsx
@@ -20,3 +20,29 @@ export const usePersistedState = (key, defaultValue) => {
   return [state, setState];
 };
 
+
+// import { useState } from "react";
+// export const usePersistedState = (key, defaultValue) => {
+//   const [state, setState] = useState(() => {
+//     const persistedState = localStorage.getItem(key);
+//     if (persistedState) {
+//       return JSON.parse(persistedState);
+//     }
+//     return defaultValue;
+//   });
+
+//   const setPersistedState = (value) => {
+//     setState(value);
+//     let serializedValue;
+//     if (typeof value === "function") {
+//       serializedValue = JSON.stringify(value(state));
+//     } else {
+//       serializedValue = JSON.stringify(value);
+//     }
+//     if (typeof localStorage !== 'undefined') {
+//       localStorage.setItem(key, serializedValue);
+
+//   }
+//   };
+//   return [state, setPersistedState];
+// };
