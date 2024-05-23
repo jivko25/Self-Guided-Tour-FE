9568f92c67dc7296e5ed7e58e453b3af2a9effb5
ajummer <makeudreamempty@gmail.com>
2024-05-23 15:02:47 +0300

    Create forgot password form

--- app/app/components/forgot-password-form/ForgotPasswordForm.jsx
+++ app/app/components/forgot-password-form/ForgotPasswordForm.jsx
@@ -1,30 +1,34 @@
-"use client"
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { Container, Typography, TextField, Button, Grid, Link } from '@mui/material';
-import LockResetIcon from '@mui/icons-material/LockReset';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
+"use client";
+import { useFormik } from "formik";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Link,
+} from "@mui/material";
+import LockResetIcon from "@mui/icons-material/LockReset";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import { emailSchema } from "@/utils/authSchemas.js";
 
-// Validation schema
-const validationSchema = Yup.object({
-  email: Yup.string()
-    .email('Invalid email address')
-    .required('Email is required'),
-});
+
+
 
 export default function ForgotPasswordForm() {
   const formik = useFormik({
-    initialValues: { email: '' },
-    validationSchema: validationSchema,
+    initialValues: { email: "" },
+    validationSchema: emailSchema,
     onSubmit: async (values) => {
       try {
         // Implement your password reset logic here
         // For example, call a function to send the reset link
         // await sendResetPasswordLink(values.email);
-        console.log('Form values:', values);
+        console.log("Form values:", values);
       } catch (error) {
-        console.error('Failed to send reset link:', error);
+        console.error("Failed to send reset link:", error);
       }
     },
   });
@@ -34,18 +38,27 @@ export default function ForgotPasswordForm() {
       <Box
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
           <LockResetIcon />
         </Avatar>
-        <Typography variant="body1" sx={{ width: '100%', textAlign: 'center', mt: 1, mb: 1 }}>
-          Please provide the email address linked to your account, and we will email you a link to reset your password.
+        <Typography
+          variant="body1"
+          sx={{ width: "100%", textAlign: "center", mt: 1, mb: 1 }}
+        >
+          Please provide the email address linked to your account, and we will
+          email you a link to reset your password
         </Typography>
-        <Box component="form" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}>
+        <Box
+          component="form"
+          onSubmit={formik.handleSubmit}
+          noValidate
+          sx={{ mt: 1, width: "100%" }}
+        >
           <TextField
             margin="normal"
             fullWidth
@@ -68,10 +81,17 @@ export default function ForgotPasswordForm() {
           >
             Send Reset Link
           </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="/login" variant="body2">
-                {"Remembered your password? "}
+          <Grid container >
+            <Grid item sx={{ display: "flex", alignItems: "center" }}>
+              <Link
+                href="/login"
+                style={{ textDecoration: "none", color: "black" }}
+                variant="body1"
+              >
+                <Box sx={{ display: "flex", alignItems: "center"  }}>
+                  <KeyboardReturnIcon sx={{ mr: 2 }} />
+                  {"Return to Login page"}
+                </Box>
               </Link>
             </Grid>
           </Grid>
