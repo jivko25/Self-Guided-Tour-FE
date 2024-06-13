8dac8dcd2b9fc8e5b57ddf382ba2e7e2a3c0bb9f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-13 09:46:02 +0300

    Create page.jsx

---
+++ app/app/sign-in/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from "../components/loginForm/LoginForm";
+
+export default function SignIn() {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+}
