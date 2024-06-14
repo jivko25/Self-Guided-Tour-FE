5fcabffc27ea49f63f63db04c89e47961fd27656
ajummer <makeudreamempty@gmail.com>
2024-06-14 12:02:10 +0300

    Add Nav Layout Wrapper

---
+++ app/app/components/Layout/NavLayout.jsx
@@ -0,0 +1,23 @@
+"use client";
+import useScreenSize from "@/app/hooks/useScreenSize.jsx";
+
+import Header from "../Header/Header.jsx";
+import HeaderMobile from "../Header/HeaderMobile.jsx";
+import Footer from "../Footer.jsx";
+
+export default function NavLayout() {
+  const screenWidth = useScreenSize();
+
+  return (
+    <div className="tablet:w-full">
+      {screenWidth <= 834 ? (
+        <HeaderMobile />
+      ) : (
+        <>
+          <Header />
+          <Footer />
+        </>
+      )}
+    </div>
+  );
+}
