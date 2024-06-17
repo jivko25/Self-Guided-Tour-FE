f457fb362d5978dfcdd5e22aefe75fd55b203ada
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-06-18 02:21:26 +0300

    fix: fixed import

--- app/app/sign-in/page.jsx
+++ app/app/sign-in/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../components/loginForm/LoginForm";
+import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function SignIn() {
   return (
