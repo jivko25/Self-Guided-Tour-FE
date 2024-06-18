2c1aa4ca551cdbcc5b5925fcfd3ea1d138929db1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-18 15:57:15 +0300

    Update TabletGuest.jsx

--- app/app/components/Header/Tablet/TabletUsers/TabletGuest.jsx
+++ app/app/components/Header/Tablet/TabletUsers/TabletGuest.jsx
@@ -1,39 +1,61 @@
 "use client";
 import Link from "next/link";
 import Button from "../../../Buttons/Button";
-
+import Btn from "../../../Buttons/Btn";
 export default function TabletGuest() {
   return (
     <nav className="flex flex-col w-full bg-neutral-50 h-[404px]">
       <Link
         href="/"
-        className="flex justify-center items-center text-center text-gray-900 font-medium text-2xl p-10"
+        className="flex justify-center items-center text-center text-gray-900 font-medium text-2xl p-6"
       >
         LOGO
       </Link>
 
       <div className="flex justify-center items-center">
         <div className="flex justify-between items-center w-full tablet:max-w-[583px]">
-          <input className="rounded-bl-[5px] rounded-tl-[5px] border border-stone-300 bg-neutral-50/opacity-80 w-full h-[36px] tablet:max-w-[547px]" type="text" />
+          <input
+            className="rounded-bl-[5px] rounded-tl-[5px] border border-stone-300 bg-neutral-50 w-full h-[36px] tablet:max-w-[547px]"
+            type="text"
+          />
           <button className="cursor-pointer bg-blue-950 rounded-tr-[5px] rounded-br-[5px] w-full h-[36px] tablet:max-w-[36px]">
             p
           </button>
         </div>
       </div>
 
-      <Link href="/menu" className="">
-        Menu
-      </Link>
-      <Link href="/explore" className="">
-        Explore
-      </Link>
-      <div className="">
-        <Link href="/sign-in" className="">
-          Sign In
-        </Link>
-        <Link href="/create-account" className="">
-          Create Account
-        </Link>
+      <div className="flex items-center justify-center w-full mt-[30px]">
+        <div className="flex flex-col items-start w-full tablet:max-w-[610px] ">
+          <Btn
+            type="button"
+            variant="transparent"
+            text="Explore"
+            link="/explore"
+          />
+          <Btn type="button" variant="transparent" text="Menu" link="/menu" />
+
+          <div className="flex mt-[30px]">
+            <div className="mx-2.5">
+              <Btn
+                type="button"
+                variant="outlined"
+                text="Sign in"
+                link="/sign-in"
+                width="w-[182px]"
+              />
+            </div>
+            <div className="mx-2.5">
+              <Btn
+                type="button"
+                variant="filled"
+                text="Create Account"
+                link="/create-account"
+                width="w-[182px]"
+
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
