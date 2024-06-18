c76b6061b19f45c5764729cd4797f7b62efd5229
Михаел Йорданов <gaws@mail.bg>
2024-06-18 10:34:02 +0300

    remove google button from home page

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 
-import ButtonGoogle from "./components/Buttons/ButtonGoogle.jsx";
-
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col h-screen w-screen">
