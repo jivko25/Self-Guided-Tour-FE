7c4a3b134356b6916b91e4bece22c24969cc5287
Михаел Йорданов <gaws@mail.bg>
2024-06-18 11:33:04 +0300

    add external login action

--- app/app/actions/authActions.js
+++ app/app/actions/authActions.js
@@ -1,75 +1,95 @@
-"use server"
+"use server";
 
 import { axiosAuth } from "@/api/axios";
 import { deleteCookie, getCookie, setCookie } from "../utils/authHelper";
 
 // Register user
 export async function registerUser(prev, formData) {
-    const fData = Object.fromEntries(formData);
-    let data = null;
-    let error = null;
+  const fData = Object.fromEntries(formData);
+  let data = null;
+  let error = null;
 
-    try {
-        const response = await axiosAuth.post("register", {
-            ...fData,
-        });
-        setCookie('session', {accessToken: response.data.accessToken, 
-                                accessTokenExpiration: response.data.accessTokenExpiration, 
-                                refreshToken: response.data.refreshToken});
-        data = true;
-    } catch (err) {
-        error = err.response?.data;
-    }
+  try {
+    const response = await axiosAuth.post("register", {
+      ...fData,
+    });
+    setCookie("session", {
+      accessToken: response.data.accessToken,
+      accessTokenExpiration: response.data.accessTokenExpiration,
+      refreshToken: response.data.refreshToken,
+    });
+    data = true;
+  } catch (err) {
+    error = err.response?.data;
+  }
 
-    return { data, error };
+  return { data, error };
 }
 
 // Login user
 export async function loginUser(prev, formData) {
-    const fData = Object.fromEntries(formData);
-    let data = null;
-    let error = null;
-    
-    try {
-        const response = await axiosAuth.post("login", {
-            ...fData,
-        });
-        setCookie('session', {accessToken: response.data.accessToken,
-                            accessTokenExpiration: response.data.accessTokenExpiration, 
-                            refreshToken: response.data.refreshToken});
-        data = true;
-    } catch (err) {
-        error = err.response?.data;
-    }
+  const fData = Object.fromEntries(formData);
+  let data = null;
+  let error = null;
 
-    return { data, error };
+  try {
+    const response = await axiosAuth.post("login", {
+      ...fData,
+    });
+    setCookie("session", {
+      accessToken: response.data.accessToken,
+      accessTokenExpiration: response.data.accessTokenExpiration,
+      refreshToken: response.data.refreshToken,
+    });
+    data = true;
+  } catch (err) {
+    error = err.response?.data;
+  }
+
+  return { data, error };
+}
+export async function externalLoginUser(response) {
+  try {
+    const { access_token } = response;
+
+    const res = await axios.post("/Auth/google-signin", {
+      idToken: access_token,
+    });
+    setCookie("session", {
+      accessToken: response.data.accessToken,
+      accessTokenExpiration: response.data.accessTokenExpiration,
+      refreshToken: response.data.refreshToken,
+    });
+  } catch (err) {
+    console.log(err.response?.data);
+  }
 }
 
 // Logout user
 export async function logoutUser() {
-    let data = null;
-    let error = null;
-    
-    try {
-        // to be checked with backend
-        // const session = getCookie('session');
-        
-        // const response = await axiosAuth.post("logout", {
-        //     userId: session.accessToken
-        // });
-        
-        deleteCookie('session');
-        
-        data = 'success';
-    } catch (err) {
-        error = err.response?.data;
-    }
+  let data = null;
+  let error = null;
 
-    return { data, error };
+  try {
+    // to be checked with backend
+    // const session = getCookie('session');
+
+    // const response = await axiosAuth.post("logout", {
+    //     userId: session.accessToken
+    // });
+
+    deleteCookie("session");
+
+    data = "success";
+  } catch (err) {
+    error = err.response?.data;
+  }
+
+  return { data, error };
 }
 
-// Returns the session state 
+// Returns the session state
 export async function getUserSession() {
-    const session = getCookie('session');
-    return session ? true : false;
+  const session = getCookie("session");
+  return session ? true : false;
 }
