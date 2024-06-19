84bf779a74c810800cd66396c6a52ffe13b5078d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-19 14:34:07 +0300

    Update page.jsx

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,8 +1,10 @@
 "use client";
+import Search from "@/app/components/Search/Search";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col h-screen w-screen">
+      <Search variant="default" /> 
     </main>
   );
 }
