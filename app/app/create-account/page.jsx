f667392e919a40d7aaab8842b8ffb113f26bc83a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-20 21:27:11 +0300

    Update page.jsx

--- app/app/create-account/page.jsx
+++ app/app/create-account/page.jsx
@@ -3,16 +3,15 @@ import RegisterForm from "../components/RegisterForm/RegisterForm";
 
 export default function SignUp() {
   return (
-    <section className="relative flex items-center justify-center flex-col h-screen w-full bg-cover ">
+    <section className="flex items-center justify-center flex-col h-screen w-full bg-cover ">
       <div
-        className="absolute top-0 left-0 w-full h-full bg-custom-image bg-cover bg-center  
-
-smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none 
-phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none 
-tablet:brightness-50 tablet:max-h-[554px]  
-web:w-full web:h-full web:tablet:max-h-none web:brightness-50"
+        className="absolute left-0 w-full h-full bg-custom-image bg-cover bg-center  z-[-5]
+        smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none smallPhone:top-0 
+        phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none phone:top-0 
+        tablet:brightness-50 tablet:max-h-[554px] tablet:top-0 
+        web:w-full web:h-full web:tablet:max-h-none web:brightness-50 web:top-[116px] "
       ></div>
-      <div className="relative w-full">
+      <div className="w-full">
         <RegisterForm />
       </div>
     </section>
