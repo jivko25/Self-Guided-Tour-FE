356e5eea2a3f0a08c5ab439e7cc0468e1836834f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-20 13:46:55 +0300

    Update page.jsx

--- app/app/sign-in/page.jsx
+++ app/app/sign-in/page.jsx
@@ -3,7 +3,9 @@ import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function SignIn() {
   return (
-    <section className="flex items-center justify-center flex-col h-screen w-full bg-custom-image bg-cover bg-opacity-10 bg-top">
+    <section className="flex items-center justify-center flex-col h-screen w-full 
+    bg-custom-image bg-cover bg-opacity-10  
+    web:bg-center ">
       <LoginForm />
     </section>
   );
