a867022e10a4bbcbcfda6abdf83b365a5ef267c7
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-06-21 19:01:18 +0300

    fix mobile navigation toggle

--- app/app/components/MobileMenuOverlay/MobileMenuOverlayUser.jsx
+++ app/app/components/MobileMenuOverlay/MobileMenuOverlayUser.jsx
@@ -1,9 +1,9 @@
 import Btn from "../Buttons/Btn.jsx";
-export default function MobileMenuOverlayUser() {
+export default function MobileMenuOverlayUser({menuOverlayVisible}) {
   return (
-    <div className="flex flex-col justify-center items-center gap-4  mt-8  ">
-      <Btn type="button" variant="transparent" text="Explore" link="/explore" />
-      <Btn type="button" variant="transparent" text="Create" link="/create" />
+    <div className="flex flex-col justify-center items-center gap-4  mt-8" onClick={menuOverlayVisible}>
+      <Btn type="button" variant="transparent" text="Explore" link="/explore" onClick={menuOverlayVisible}/>
+      <Btn type="button" variant="transparent" text="Create" link="/create" onClick={menuOverlayVisible}/>
       <Btn
         type="button"
         variant="transparent"
@@ -11,7 +11,7 @@ export default function MobileMenuOverlayUser() {
         link="/my-tours"
       />
       <div className="w-11/12 flex flex-col gap-2 my-8">
-        <Btn fullWidth text="Menu" link="/menu" />
+        <Btn fullWidth text="Menu" link="/menu" onClick={menuOverlayVisible}/>
       </div>
     </div>
   );
