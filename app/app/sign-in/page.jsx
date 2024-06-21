bd8096b873b71651b8386f9c46bb2691b31f74e9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 12:58:03 +0300

    Update page.jsx

--- app/app/sign-in/page.jsx
+++ app/app/sign-in/page.jsx
@@ -3,15 +3,20 @@ import LoginForm from "../components/LoginForm/LoginForm";
 
 export default function SignIn() {
   return (
-    <section className="flex items-center justify-center flex-col h-screen w-full bg-cover ">
+    <section className="flex relative items-center justify-center flex-col  w-full bg-cover min-h-screen">
       <div
         className="absolute left-0 w-full h-full bg-custom-image bg-cover bg-center  z-[-5]
-        smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none smallPhone:top-0 
-        phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none phone:top-0 
-        tablet:brightness-50 tablet:max-h-[554px] tablet:top-0 
-        web:w-full web:h-full web:tablet:max-h-none web:brightness-50 web:top-[116px] "
+      smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none smallPhone:top-0 
+      phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none phone:top-0 
+      tablet:brightness-50 tablet:max-h-[554px] tablet:top-0 
+      web:w-full web:h-full web:tablet:max-h-none web:brightness-50 web:top-0"
       ></div>
-      <div className="w-full">
+      <div className="w-full absolute   
+        tablet:top-[150px]
+        smallPhone:top-[70px]
+        phone:top-[70px]
+        web:top-[70px]
+        ">
         <LoginForm />
       </div>
     </section>
