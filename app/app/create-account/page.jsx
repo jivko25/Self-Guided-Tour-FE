83b9e920b711e2e518936dd9a93e3b8eb731c3ba
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 13:25:03 +0300

    Update page.jsx

--- app/app/create-account/page.jsx
+++ app/app/create-account/page.jsx
@@ -12,11 +12,11 @@ export default function SignUp() {
       web:w-full web:h-full web:tablet:max-h-none web:brightness-50 web:top-0"
       ></div>
       <div className="w-full h-full absolute   
-        tablet:top-[150px]
-        smallPhone:top-[70px]
-        phone:top-[70px]
-        web:top-[70px]
-        ">
+                      tablet:top-[150px]
+                      smallPhone:top-[70px]
+                      phone:top-[70px]
+                      web:top-[70px]
+                      ">
         <RegisterForm />
       </div>
     </section>
