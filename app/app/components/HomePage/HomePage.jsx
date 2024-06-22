e4f702700fbbc5c940ca2ec435c0c326567d31e3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-22 22:08:39 +0300

    Section heading for web is done

--- app/app/components/HomePage/HomePage.jsx
+++ app/app/components/HomePage/HomePage.jsx
@@ -4,42 +4,31 @@ import Search from "@/app/components/Search/Search";
 
 function HomePage() {
   return (
-    <div className="flex flex-col items-center w-full min-h-screen">
+    <div className="flex flex-col w-full min-h-screen">
       <div
-        className="absolute brightness-75 top-0 left-0 w-full h-full bg-custom-image bg-cover bg-center z-[-5]
-      smallPhone:h-[222px] smallPhone:phone:h-none 
-      phone:h-[245px]  phone:tablet:h-none 
-      tablet:h-[554px]
-      web:w-full web:h-[655px] web:tablet:h-none"
+        className="absolute top-0 left-0 w-full h-full bg-custom-image bg-cover bg-center z-[-5]
+      smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none 
+      phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none 
+      tablet:brightness-50 tablet:max-h-[554px]
+      web:w-full web:h-[655px] web:tablet:max-h-none web:brightness-50 "
       ></div>
-      <section className="flex flex-col items-center smallPhone:relative phone:relative justify-center smallPhone:h-[222px] phone:h-[245px] tablet:py-[150px] tablet:h-[554px] web:h-[535px] w-full ">
+      <section className="flex flex-col items-center web:justify-center web:h-[535px] w-full ">
         <h1
-          className=" text-2xl font-bold text-center text-white font-['Inter Tight']
-        web:text-[64px] web:mb-[45px]
-        tablet:text-5xl tablet:mb-[50px]
-        phone:text-2xl phone:mb-[10px]
-        smallPhone:text-2xl smallPhone:mb-[15px]
+          className="text-center text-white font-['Inter Tight']
+        web:text-[64px] web:font-bold web:mb-[35px]
         "
         >
           Heading
         </h1>
         <p
-          className="text-base font-medium text-center text-white 
-        web:text-[32px] web:mb-[65px]
-        tablet:text-2xl tablet:mb-[70px]
-        phone:text-base phone:mb-[40px]
-        smallPhone:text-base smallPhone:mb-[40px]
+          className="text-center text-white 
+        web:text-[32px] web:font-medium web:mb-[55px]
         "
         >
           Subheading
         </p>
-        <div
-          className="web:mb-[70px] web:static web:phone:absolute-none tablet:static tablet:mb-[0px]
-        phone:absolute phone:bottom-[-20px]
-        smallPhone:absolute smallPhone:bottom-[-25px]"
-        >
+        <div className="web:mb-[35px]">
           <Search variant="default" />
-          
         </div>
       </section>
     </div>
