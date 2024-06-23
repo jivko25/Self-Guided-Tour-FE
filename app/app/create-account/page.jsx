e846a755dec62a663dd70060eb75227291f48f4c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-23 12:44:57 +0300

    Update page.jsx

--- app/app/create-account/page.jsx
+++ app/app/create-account/page.jsx
@@ -5,12 +5,11 @@ export default function SignUp() {
   return (
     <section className="flex relative items-center justify-center flex-col w-full bg-cover min-h-screen">
       <div
-        className="absolute left-0 w-full h-full bg-custom-image bg-cover bg-center  z-[-5]
-      smallPhone:max-h-[245px] smallPhone:brightness-50 smallPhone:phone:max-h-none smallPhone: 
-      phone:max-h-[245px] phone:brightness-50 phone:tablet:max-h-none phone:top-0 
-      tablet:brightness-50 tablet:max-h-[554px] tablet:top-0 
-      web:w-full web:h-full web:tablet:max-h-none web:brightness-50 web:top-0
-      max-h-[245px] brightness-50 top-0"
+        className="absolute top-0 left-0 w-full h-full bg-custom-image bg-cover bg-center z-[-5]
+      smallPhone:max-h-[245px] smallPhone:phone:max-h-none 
+      phone:max-h-[245px] phone:tablet:max-h-none 
+      tablet:max-h-[554px] 
+      web:w-full web:h-full web:tablet:max-h-none "
       ></div>
       <div className="w-full min-h-screen pb-16 absolute   
                       tablet:top-[150px]
