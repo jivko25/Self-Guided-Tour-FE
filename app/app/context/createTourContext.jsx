0d2ce2abf3e3fca87f7cd135720b4eec8b6d1782
ajummer <makeudreamempty@gmail.com>
2024-06-24 14:27:51 +0300

    add create tour context

---
+++ app/app/context/createTourContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState } from 'react';
+
+const CreateTourContext = createContext();
+
+export const CreateTourProvider = ({ children }) => {
+  const [step, setStep] = useState(0);
+  const [formData, setFormData] = useState({
+    step1Data: '',
+    step2Data: '',
+    step3Data: '',
+  });
+
+  const nextStep = () => setStep((prevStep) => prevStep + 1);
+  const prevStep = () => setStep((prevStep) => prevStep - 1);
+
+  const updateFormData = (newData) => {
+    setFormData((prevData) => ({ ...prevData, ...newData }));
+  };
+
+  return (
+    <CreateTourContext.Provider
+      value={{ step, formData, nextStep, prevStep, updateFormData }}
+    >
+      {children}
+    </CreateTourContext.Provider>
+  );
+};
+
+export const useCreateTour = () => {
+  return useContext(CreateTourContext);
+};
