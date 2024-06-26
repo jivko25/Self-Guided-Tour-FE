fbeb842aa5dc7a153a18778b351de023c41de1f3
ajummer <makeudreamempty@gmail.com>
2024-06-26 12:53:35 +0300

    Create Stepper Component

--- app/app/components/Stepper/Stepper.jsx
+++ app/app/components/Stepper/Stepper.jsx
@@ -1,54 +1,40 @@
 import Image from "next/image";
+<<<<<<< HEAD
 import { useCreateTour } from "../../context/createTourContext.jsx";
+=======
+>>>>>>> 31312e9 (Create Stepper Component)
 import Create from "../../public/svg/create-stepper.svg";
 import Location from "../../public/svg/location-stepper.svg";
 import Visualize from "../../public/svg/image-stepper.svg";
 import Overview from "../../public/svg/checkbox-stepper.svg";
 
-const steps = [
-  { src: Create, alt: "Create your tour", text: "Create your tour" },
-  { src: Location, alt: "Plan the route", text: "Plan the route" },
-  { src: Visualize, alt: "Describe and shoot", text: "Describe and shoot" },
-  { src: Overview, alt: "Review and publish", text: "Review and publish" },
-];
-
 export default function Stepper() {
-  const { step, goToStep } = useCreateTour();
-
   return (
     <nav
-      className="w-full h-[140px] flex justify-center mb-2 mt-8 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200 tablet:mt-24 web:mt-8"
+      className="w-full h-[88px] flex justify-center my-6 gap-2 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200"
       aria-label="Progress"
     >
-      <ul
-        className={`flex justify-between items-center w-5/6 tablet:justify-center tablet:gap-12 ${
-          step === 1 || step === 2 ? "web:justify-start" : ""
-        }`}
-      >
-        {steps.map((stepData, index) => (
-          <li
-            key={index}
-            className="flex flex-col items-center cursor-pointer"
-            onClick={() => goToStep(index)}
-          >
-            <span
-              className={`w-10 h-10 ${
-                index <= step ? "bg-yellow-500" : "bg-neutral-50"
-              } rounded-full flex items-center justify-center ${
-                index <= step ? "" : "border border-yellow-500"
-              } tablet:w-[60px] tablet:h-[60px]`}
-            >
-              <Image
-                src={stepData.src}
-                alt={stepData.alt}
-                className="w-6 h-6"
-              />
-            </span>
-            <p className="hidden mt-2 text-blue-950 text-sm font-normal leading-none tablet:block">
-              {stepData.text}
-            </p>
-          </li>
-        ))}
+      <ul className="flex justify-between gap-8 items-center w-5/6">
+        <li className="w-10 h-10 flex justify-center items-center">
+          <span className="w-10 h-10 bg-neutral-50 rounded-full border border-yellow-500 flex items-center justify-center">
+            <Image src={Create} alt="Create" className="w-6 h-6" />
+          </span>
+        </li>
+        <li className="w-10 h-10 flex justify-center items-center">
+          <span className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center">
+            <Image src={Location} alt="Location" className="w-6 h-6" />
+          </span>
+        </li>
+        <li className="w-10 h-10 flex justify-center items-center">
+          <span className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center">
+            <Image src={Visualize} alt="Visualize" className="w-6 h-6" />
+          </span>
+        </li>
+        <li className="w-10 h-10 flex justify-center items-center">
+          <span className="w-10 h-10 bg-neutral-50 rounded-full border border-yellow-500 flex items-center justify-center">
+            <Image src={Overview} alt="Overview" className="w-6 h-6" />
+          </span>
+        </li>
       </ul>
     </nav>
   );
