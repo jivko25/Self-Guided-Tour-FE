005f4125dec6e968e584e4686995cba8176a658f
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-06-26 19:54:14 +0300

    more adjustments on web view

--- app/app/components/CreateTourSteps/Step2.jsx
+++ app/app/components/CreateTourSteps/Step2.jsx
@@ -20,22 +20,22 @@ const Step2 = () => {
 
   return (
     <section className="w-[100%] flex flex-col align-center gap-4 text-[14px]
-                        mb-[120px] font-medium text-[#081120] text-[14px] web:align-left web:w-[582px]
+                        mb-[120px] font-medium text-[#081120] text-[14px] 
                         px-[8px] phone:px-[16px] tablet:mt-[115px] tablet:px-[125px] web:mt-0 web:px-[36px]">
-      <header className="flex flex-row justify-between">
+      <header className="flex flex-row justify-between web:mr-[900px]">
         <h2 className="text-[20px]">Plan your route</h2>
         <span className="mt-[7px] tablet:mt-0 text-[#E8B600]">Step 2 of 4</span>
       </header>
-      <p className="max-w-[296px] tablet:max-w-[425px] font-normal">Choose locations on the map, give them titles and add them to your tour.</p>
+      <p className="max-w-[296px] tablet:max-w-[425px] font-normal web:mr-[900px]">Choose locations on the map, give them titles and add them to your tour.</p>
       <section className="h-[250px] phone:h-[297px] tablet:h-[476px] mb-[20px] web:w-[834px] web:h-[582px] web:absolute web:right-[40px]">
         <GoogleMapsComponent />
       </section>
-      <section className="flex flex-wrap gap-6 web:w-[582px]">
+      <section className="flex flex-wrap gap-6 web:max-w-[582px] web:mr-[900px]">
         <InputField classes="w-[100%] shrink-0" label='Location' name='location'/>
         <InputField classes="w-[100%] tablet:flex-1 shrink" label='Latitude' name='latitude'/>
         <InputField classes="w-[100%] tablet:flex-1 shrink" label='Longitude'name='longitude'/>
       </section>
-        <Btn className="text-[16px] border-b-2 border-b-[#E8B600] w-[177px] h-[43px] self-center" variant="transparent" text="Next" onClick={nextStep} />
+        <Btn className="text-[16px] border-b-2 border-b-[#E8B600] w-[177px] h-[43px] self-center web:mr-[900px]" variant="transparent" text="Next" onClick={nextStep} />
     </section>
   );
 };
