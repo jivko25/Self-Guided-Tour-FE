cd2795b0de4d260af8aecf2c41a0c7e5f60ee5cf
ajummer <makeudreamempty@gmail.com>
2024-06-26 13:00:10 +0300

    Add Functionality for navigation

--- app/app/components/Stepper/Stepper.jsx
+++ app/app/components/Stepper/Stepper.jsx
@@ -1,36 +1,47 @@
 import Image from "next/image";
+import { useCreateTour } from "../../context/createTourContext.jsx";
 import Create from "../../public/svg/create-stepper.svg";
 import Location from "../../public/svg/location-stepper.svg";
 import Visualize from "../../public/svg/image-stepper.svg";
 import Overview from "../../public/svg/checkbox-stepper.svg";
 
+const steps = [
+  { src: Create, alt: "Create" },
+  { src: Location, alt: "Location" },
+  { src: Visualize, alt: "Visualize" },
+  { src: Overview, alt: "Overview" },
+];
+
 export default function Stepper() {
+  const { step, goToStep } = useCreateTour();
+
   return (
     <nav
-      className="w-full h-[88px] flex justify-center my-6 gap-2 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200"
+      className="w-full h-[88px] flex justify-center mb-2 gap-2 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200"
       aria-label="Progress"
     >
-      <ul className="flex justify-between gap-8 items-center w-5/6">
-        <li className="w-10 h-10 flex justify-center items-center">
-          <span className="w-10 h-10 bg-neutral-50 rounded-full border border-yellow-500 flex items-center justify-center">
-            <Image src={Create} alt="Create" className="w-6 h-6" />
-          </span>
-        </li>
-        <li className="w-10 h-10 flex justify-center items-center">
-          <span className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center">
-            <Image src={Location} alt="Location" className="w-6 h-6" />
-          </span>
-        </li>
-        <li className="w-10 h-10 flex justify-center items-center">
-          <span className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center">
-            <Image src={Visualize} alt="Visualize" className="w-6 h-6" />
-          </span>
-        </li>
-        <li className="w-10 h-10 flex justify-center items-center">
-          <span className="w-10 h-10 bg-neutral-50 rounded-full border border-yellow-500 flex items-center justify-center">
-            <Image src={Overview} alt="Overview" className="w-6 h-6" />
-          </span>
-        </li>
+      <ul className="flex justify-center gap-8 items-center w-5/6">
+        {steps.map((stepData, index) => (
+          <li
+            key={index}
+            className="w-10 h-10 flex justify-center items-center cursor-pointer"
+            onClick={() => goToStep(index)}
+          >
+            <span
+              className={`w-10 h-10 ${
+                index <= step ? "bg-yellow-500" : "bg-neutral-50"
+              } rounded-full flex items-center justify-center ${
+                index <= step ? "" : "border border-yellow-500"
+              }`}
+            >
+              <Image
+                src={stepData.src}
+                alt={stepData.alt}
+                className="w-6 h-6"
+              />
+            </span>
+          </li>
+        ))}
       </ul>
     </nav>
   );
