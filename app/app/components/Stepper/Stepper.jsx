3a7beab652d3b0780efd078432b0cb089673c777
ajummer <makeudreamempty@gmail.com>
2024-06-26 21:26:23 +0300

    Add responsive design

--- app/app/components/Stepper/Stepper.jsx
+++ app/app/components/Stepper/Stepper.jsx
@@ -6,10 +6,10 @@ import Visualize from "../../public/svg/image-stepper.svg";
 import Overview from "../../public/svg/checkbox-stepper.svg";
 
 const steps = [
-  { src: Create, alt: "Create" },
-  { src: Location, alt: "Location" },
-  { src: Visualize, alt: "Visualize" },
-  { src: Overview, alt: "Overview" },
+  { src: Create, alt: "Create your tour", text: "Create your tour" },
+  { src: Location, alt: "Plan the route", text: "Plan the route" },
+  { src: Visualize, alt: "Describe and shoot", text: "Describe and shoot" },
+  { src: Overview, alt: "Review and publish", text: "Review and publish" },
 ];
 
 export default function Stepper() {
@@ -17,14 +17,18 @@ export default function Stepper() {
 
   return (
     <nav
-      className="w-full h-[88px] flex justify-center mb-2 gap-2 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200"
+      className="w-full h-[140px] flex justify-center mb-2 mt-8 bg-neutral-50 rounded-tl-[5px] rounded-tr-[5px] border-b border-gray-200 tablet:mt-24 web:mt-8"
       aria-label="Progress"
     >
-      <ul className="flex justify-center gap-8 items-center w-5/6">
+      <ul
+        className={`flex justify-between items-center w-5/6 tablet:justify-center tablet:gap-12 ${
+          step === 1 || step === 2 ? "web:justify-start" : ""
+        }`}
+      >
         {steps.map((stepData, index) => (
           <li
             key={index}
-            className="w-10 h-10 flex justify-center items-center cursor-pointer"
+            className="flex flex-col items-center cursor-pointer"
             onClick={() => goToStep(index)}
           >
             <span
@@ -32,7 +36,7 @@ export default function Stepper() {
                 index <= step ? "bg-yellow-500" : "bg-neutral-50"
               } rounded-full flex items-center justify-center ${
                 index <= step ? "" : "border border-yellow-500"
-              }`}
+              } tablet:w-[60px] tablet:h-[60px]`}
             >
               <Image
                 src={stepData.src}
@@ -40,6 +44,9 @@ export default function Stepper() {
                 className="w-6 h-6"
               />
             </span>
+            <p className="hidden mt-2 text-blue-950 text-sm font-normal leading-none tablet:block">
+              {stepData.text}
+            </p>
           </li>
         ))}
       </ul>
