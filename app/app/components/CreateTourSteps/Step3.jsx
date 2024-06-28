f0ccec77d1f0486b93b94995b51e68011bdb5fbc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-28 17:24:38 +0300

    Update Step3.jsx

--- app/app/components/CreateTourSteps/Step3.jsx
+++ app/app/components/CreateTourSteps/Step3.jsx
@@ -67,9 +67,9 @@ const Step3 = () => {
   };
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <div className="flex justify-between h-full min-h-[691px] w-full">
-        <div className="flex-1 flex flex-col items-center justify-center w-full h-full min-h-[691px] overflow-y-auto ">
+    <div className="flex flex-col h-screen w-full">
+      <div className="flex justify-between w-full h-full max-h-[691px]">
+        <div className="flex-1 flex flex-col items-center justify-start max-h-[691px] w-full overflow-y-auto">
           <div className="flex items-center justify-between w-full max-w-[581px] pt-[50px] pb-[30px]">
             <div>
               <h4 className="text-gray-900 text-2xl font-medium leading-9">
@@ -86,108 +86,118 @@ const Step3 = () => {
           </div>
 
           {/* Inputs */}
-          <div className="flex items-center flex-col mb-[30px] w-full max-w-[581px]">
-            <label
-              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
-              htmlFor="locationName"
-            >
-              Location Name
-            </label>
-            <input
-              id="locationName"
-              name="locationName"
-              type="text"
-              value={inputs.locationName}
-              onChange={handleChange}
-              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[581px] h-[60px]"
-              required
-            />
-          </div>
-
-          <div className="flex items-center flex-col w-full mb-[20px] max-w-[581px]">
-            <label
-              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
-              htmlFor="locationDescription"
-            >
-              Location Description
-            </label>
-            <textarea
-              id="locationDescription"
-              name="locationDescription"
-              value={inputs.locationDescription}
-              onChange={handleChange}
-              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[582px] h-[200px]"
-              required
-            />
-          </div>
-
-          <div className="flex items-center flex-col mb-[30px] w-full max-w-[581px]">
-            <label
-              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
-              htmlFor="addInput"
-            >
-              Add Fields
-            </label>
-            <div className="flex justify-between w-full max-w-[581px] h-[60px] bg-neutral-50 rounded-[5px] border border-stone-300">
-              <input
-                id="addInput"
-                name="addFields"
-                type="file"
-                multiple
-                className="hidden"
-                onChange={handleChange}
-              />
-              <div className="flex w-full max-w-[453px] h-full pl-[10px] items-center">
-                <span className="text-gray-500">
-                  You can upload images and audio files up to 5MB
-                </span>
-              </div>
+          <div className="flex items-center flex-col w-full max-w-[581px]">
+            <div className="flex items-center flex-col mb-[30px] w-full">
               <label
-                htmlFor="addInput"
-                className="flex bg-blue-950 text-white border-2 rounded-md border-transparent justify-center items-center text-center font-medium h-full w-full max-w-[128px] cursor-pointer"
+                className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+                htmlFor="locationName"
               >
-                Upload
+                Location Name
               </label>
+              <input
+                id="locationName"
+                name="locationName"
+                type="text"
+                value={inputs.locationName}
+                onChange={handleChange}
+                className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[581px] h-[60px]"
+                required
+              />
             </div>
-          </div>
 
-          {/* IMAGES */}
-          <div className="flex items-center justify-center w-full max-w-[581px] h-full">
-            <div className="flex flex-row flex-wrap justify-around">
-              {inputs.addFields.length > 0 &&
-                inputs.addFields.map((file, index) => (
-                  <div key={index} className="flex flex-row  mb-4">
-                    {isImage(file) && (
-                      <Image
-                        src={URL.createObjectURL(file)}
-                        width={150}
-                        height={150}
-                        alt="Uploaded Media"
-                      />
-                    )}
-                  </div>
-                ))}
+            <div className="flex items-center flex-col w-full mb-[20px]">
+              <label
+                className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+                htmlFor="locationDescription"
+              >
+                Location Description
+              </label>
+              <textarea
+                id="locationDescription"
+                name="locationDescription"
+                value={inputs.locationDescription}
+                onChange={handleChange}
+                className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[582px] h-[200px]"
+                required
+              />
             </div>
-          </div>
 
-          {/* VIDEO  */}
-          <div className="flex flex-row items-center justify-center w-full h-full flex-wrap p-[10px]">
-            {inputs.addFields.length > 0 &&
-              inputs.addFields.map((file, index) => (
-                <div
-                  key={index}
-                  className="flex flex-row items-center justify-center mb-4"
-                >
-                  {isVideo(file) && (
-                    <ReactPlayer
-                      url={URL.createObjectURL(file)}
-                      width={150}
-                      height={150}
-                      controls={true}
-                    />
-                  )}
+            <div className="flex items-center flex-col mb-[30px] w-full">
+              <label
+                className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+                htmlFor="addInput"
+              >
+                Add Fields
+              </label>
+              <div className="flex justify-between w-full max-w-[581px] h-[60px] bg-neutral-50 rounded-[5px] border border-stone-300">
+                <input
+                  id="addInput"
+                  name="addFields"
+                  type="file"
+                  multiple
+                  className="hidden"
+                  onChange={handleChange}
+                />
+                <div className="flex w-full max-w-[453px] h-full pl-[10px] items-center">
+                  <span className="text-gray-500">
+                    You can upload images and audio files up to 5MB
+                  </span>
                 </div>
-              ))}
+                <label
+                  htmlFor="addInput"
+                  className="flex bg-blue-950 text-white border-2 rounded-md border-transparent justify-center items-center text-center font-medium h-full w-full max-w-[128px] cursor-pointer"
+                >
+                  Upload
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center w-full max-w-[581px] h-full">
+            {/* IMAGES */}
+
+            <div className="flex items-center justify-center w-full h-full">
+              <div className="flex flex-row flex-wrap justify-between">
+                {inputs.addFields.length > 0 &&
+                  inputs.addFields.map((file, index) => (
+                    <div key={index} className="flex flex-row  mb-4">
+                      {isImage(file) && (
+                        <Image
+                          src={URL.createObjectURL(file)}
+                          width={150}
+                          height={100}
+                          background-position="center"
+                          object-fit="cover"
+                          background-size="contain"
+                          alt="Uploaded Media"
+                        />
+                      )}
+                    </div>
+                  ))}
+              </div>
+            </div>
+
+            {/* VIDEO  */}
+            <div className="flex items-center justify-center w-full h-full">
+              <div className="flex  flex-row flex-wrap justify-between">
+                {inputs.addFields.length > 0 &&
+                  inputs.addFields.map((file, index) => (
+                    <div key={index} className="flex flex-row  mb-4">
+                      {isVideo(file) && (
+                        <ReactPlayer
+                          url={URL.createObjectURL(file)}
+                          width={150}
+                          height={100}
+                          background-position="center"
+                          object-fit="cover"
+                          background-size="contain"
+                          controls={true}
+                        />
+                      )}
+                    </div>
+                  ))}
+              </div>
+            </div>
           </div>
         </div>
 
