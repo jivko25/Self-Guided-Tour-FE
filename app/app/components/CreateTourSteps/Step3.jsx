41bb53c8aa873fc1e8fd05204d5e278b12649444
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-28 14:41:04 +0300

    Update Step3.jsx

--- app/app/components/CreateTourSteps/Step3.jsx
+++ app/app/components/CreateTourSteps/Step3.jsx
@@ -1,60 +1,78 @@
-"use client"
+"use client";
 import { useCreateTour } from "@/app/context/createTourContext.jsx";
 import { useState, useEffect } from "react";
 import Btn from "../Buttons/Btn.jsx";
-import InputField from "../InputField.jsx";
+import Image from "next/image";
+import ReactPlayer from "react-player"; //npm install react-player
+import { usePathname } from "next/navigation";
+import GoogleMapsComponent from "../GoogleMapsComponent/GoogleMapsComponent.js";
 
 const Step3 = () => {
+  const pathname = usePathname();
+
   const { formData, updateFormData, prevStep } = useCreateTour();
   const [inputs, setInputs] = useState({
     locationName: formData.step3Data.locationName || "",
     locationDescription: formData.step3Data.locationDescription || "",
-    addFields: formData.step3Data.addFields || "",
+    addFields: formData.step3Data.addFields || [],
   });
 
   useEffect(() => {
     setInputs({
       locationName: formData.step3Data.locationName || "",
       locationDescription: formData.step3Data.locationDescription || "",
-      addFields: formData.step3Data.addFields || "",
+      addFields: formData.step3Data.addFields || [],
     });
   }, [formData.step3Data]);
 
   const handleFinish = () => {
-    alert("Form submitted!")
-  }
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInputs((prevInputs) => ({
-      ...prevInputs,
-      [name]: value,
-    }));
-    updateFormData({step3Data: {...inputs, [name]:value}})
+    alert("Form submitted!");
   };
 
-  // const { formData, updateFormData, prevStep } = useCreateTour();
-  // const [input, setInput] = useState(formData.step3Data);
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    if (name === "addFields" && files) {
+      const fileArray = Array.from(files);
+      setInputs((prevInputs) => ({
+        ...prevInputs,
+        addFields: [...prevInputs.addFields, ...fileArray],
+      }));
+      updateFormData({
+        step3Data: {
+          ...inputs,
+          addFields: [...inputs.addFields, ...fileArray],
+        },
+      });
+    } else {
+      setInputs((prevInputs) => ({
+        ...prevInputs,
+        [name]: value,
+      }));
+      updateFormData({ step3Data: { ...inputs, [name]: value } });
+    }
+  };
 
-  // useEffect(() => {
-  //   setInput(formData.step3Data);
-  // }, [formData.step3Data]);
+  // Function to check if a file is an image
+  const isImage = (file) => file.type.startsWith("image");
 
-  // const handleFinish = () => {
-  //   alert("Form submitted!");
-  // };
+  // Function to check if a file is a video
+  const isVideo = (file) => file.type.startsWith("video");
 
-  // const handleChange = (e) => {
-  //   const newValue = e.target.value;
-  //   setInput(newValue);
-  //   updateFormData({ step3Data: newValue });
-  // };
+  const handleMapClick = (newData) => {
+    updateFormData({
+      ...formData,
+      step3Data: { ...formData.step3Data, ...newData },
+    });
+    console.log(formData);
+  };
 
   return (
     <div className="flex flex-col h-full w-full">
-      <div className="flex h-full max-h-[691px]">
-        <div className="flex items-center justify-between flex-col w-full max-w-[582px]  mx-[50px] my-[10px]">
-          <div className="flex items-center justify-between w-full mb-[20px]">
+      <div className="flex justify-between h-full max-h-[691px] w-full">
+
+        <div className="flex-1 flex flex-col items-center justify-center w-full overflow-auto">
+
+          <div className="flex items-center justify-between w-full max-w-[581px] mt-[50px] mb-[30px]">
             <div>
               <h4 className="text-gray-900 text-2xl font-medium leading-9">
                 Describe and shoot
@@ -70,10 +88,10 @@ const Step3 = () => {
           </div>
 
           {/* Inputs */}
-          <div className="flex flex-col mb-[20px] w-full">
+          <div className="flex items-center flex-col mb-[30px] w-full max-w-[581px]">
             <label
-              className="text-gray-900 text-base font-medium leading-normal"
-              htmlFor=""
+              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+              htmlFor="locationName"
             >
               Location Name
             </label>
@@ -83,80 +101,102 @@ const Step3 = () => {
               type="text"
               value={inputs.locationName}
               onChange={handleChange}
-              // value={input}
-              // onChange={handleChange}
-              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[581px] h-[60px] "
+              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[581px] h-[60px]"
               required
             />
           </div>
-          <div className="flex flex-col w-full mb-[20px]">
+
+          <div className="flex items-center flex-col w-full mb-[20px] max-w-[581px]">
             <label
-              className="text-gray-900 text-base font-medium leading-normal"
-              htmlFor=""
+              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+              htmlFor="locationDescription"
             >
               Location Description
             </label>
             <textarea
               id="locationDescription"
               name="locationDescription"
-              type="textarea"
               value={inputs.locationDescription}
               onChange={handleChange}
-              // value={input}
-              // onChange={handleChange}
-              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[582px] h-[200px] "
+              className="bg-neutral-50 rounded border border-stone-300 w-full max-w-[582px] h-[200px]"
               required
             />
           </div>
-          <div className="flex flex-col mb-[20px] w-full">
+
+          <div className="flex items-center flex-col mb-[30px] w-full max-w-[581px]">
             <label
-              className="text-gray-900 text-base font-medium leading-normal"
-              htmlFor=""
+              className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+              htmlFor="addInput"
             >
               Add Fields
             </label>
-            <div className="flex justify-between w-full max-w-[581px] h-[60px]  bg-neutral-50 rounded-[5px] border border-stone-300 ">
+            <div className="flex justify-between w-full max-w-[581px] h-[60px] bg-neutral-50 rounded-[5px] border border-stone-300">
               <input
-                id="addFields"
+                id="addInput"
                 name="addFields"
                 type="file"
-                placeholder="You can upload images and audio files up to 5MB"
-                value={inputs.addFields}
+                multiple
+                className="hidden"
                 onChange={handleChange}
-                // value={input}
-                // onChange={handleChange}
-                className={`web:w-full web:max-w-[453px] web:h-full pl-[10px]`}
-                required
               />
-
-              <button
-                className="flex bg-blue-950 text-white border-2 rounded-md border-transparent justify-center items-center text-center font-medium web:h-full web:w-full web:max-w-[128px]"
-                type="submit"
+              <div className="flex w-full max-w-[453px] h-full pl-[10px] items-center">
+                <span className="text-gray-500">
+                  You can upload images and audio files up to 5MB
+                </span>
+              </div>
+              <label
+                htmlFor="addInput"
+                className="flex bg-blue-950 text-white border-2 rounded-md border-transparent justify-center items-center text-center font-medium h-full w-full max-w-[128px] cursor-pointer"
               >
                 Upload
-              </button>
+              </label>
             </div>
+          </div>
+
+          {/* VIDEO IMAGES */}
+          <div className="flex flex-col items-center justify-center">
+            {inputs.addFields.length > 0 &&
+              inputs.addFields.map((file, index) => (
+                <div
+                  key={index}
+                  className="flex flex-row items-center justify-center mb-4"
+                >
+                  {isImage(file) && (
+                    <Image
+                      src={URL.createObjectURL(file)}
+                      width={150}
+                      height={150}
+                      alt="Uploaded Media"
+                    />
+                  )}
+                  
+                  {isVideo(file) && (
+                    <ReactPlayer
+                      url={URL.createObjectURL(file)}
+                      controls={true}
+                    />
+                  )}
+                </div>
+              ))}
           </div>
         </div>
 
         {/* MAP */}
-        <div className="w-full max-w-[1128px]  rounded-[5px]">
-          <img
-            className="w-full h-full rounded-[5px]"
-            src="https://images.unsplash.com/photo-1524661135-423995f22d0b?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
+        <div className="flex flex-2 w-full max-w-[1128px] rounded-[5px]">
+          <GoogleMapsComponent handleMapClick={handleMapClick} />
         </div>
+
       </div>
       <div className="w-full h-[138px] bg-neutral-50 border-t border-gray-200 justify-start items-center flex gap-[324px]">
         <Btn
           variant="transparent"
-          className="text-center text-gray-900 text-base font-semibold px-4 py-3 w-32 h-[43px] justify-center items-center inline-flex  border-b-2 border-neutral-50"
+          className="text-center text-gray-900 text-base font-semibold px-4 py-3 w-32 h-[43px] justify-center items-center inline-flex border-b-2 border-neutral-50"
           text="Prev"
           onClick={prevStep}
         />
         <Btn
           variant="fullWidth"
-          className="text-center text-gray-900 text-base font-semibold px-4 py-3 w-32 h-[43px] justify-center items-center inline-flex  border-b-2 border-yellow-500"
+          className="text-center text-gray-900 text-base font-semibold px-4 py-3 w-32 h-[43px] justify-center items-center inline-flex border-b-2 border-yellow-500"
           text="Next"
           onClick={handleFinish}
         />
@@ -166,3 +206,8 @@ const Step3 = () => {
 };
 
 export default Step3;
+
+
+
+
+
