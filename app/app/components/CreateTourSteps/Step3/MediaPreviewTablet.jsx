085c363d8684be5ffc6fa7d1630f89ecce331a6b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-30 01:20:17 +0300

    Update MediaPreviewTablet.jsx

--- app/app/components/CreateTourSteps/Step3/MediaPreviewTablet.jsx
+++ app/app/components/CreateTourSteps/Step3/MediaPreviewTablet.jsx
@@ -2,51 +2,49 @@
 import Image from "next/image";
 import ReactPlayer from "react-player";
 import React from "react";
+
 function MediaPreviewTablet({ inputs, isImage, isVideo }) {
   return (
     <>
       {/* IMAGES */}
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="flex flex-row flex-wrap justify-between">
-          {inputs.addFields.length > 0 &&
-            inputs.addFields.map((file, index) => (
-              <div key={index} className="flex flex-row  mb-4">
-                {isImage(file) && (
-                  <Image
-                    src={URL.createObjectURL(file)}
-                    width={150}
-                    height={100}
-                    background-position="center"
-                    object-fit="cover"
-                    background-size="contain"
-                    alt="Uploaded Media"
-                  />
-                )}
-              </div>
-            ))}
-        </div>
+      <div className="grid grid-cols-2 gap-4 w-full h-full">
+        {inputs.addFields.length > 0 &&
+          inputs.addFields.map((file, index) => (
+            <div
+              key={index}
+              className={`mb-4 ${index === 0 ? "col-span-2" : ""}`}
+            >
+              {isImage(file) && (
+                <Image
+                  src={URL.createObjectURL(file)}
+                  width={index === 0 ? 858 : 279}
+                  height={240}
+                  className={`rounded-[5px] ${
+                    index === 0 ? "w-full h-60" : "w-[279px] h-60"
+                  }`}
+                  alt="Uploaded Media"
+                />
+              )}
+            </div>
+          ))}
       </div>
 
-      {/* VIDEO  */}
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="flex  flex-row flex-wrap justify-between">
-          {inputs.addFields.length > 0 &&
-            inputs.addFields.map((file, index) => (
-              <div key={index} className="flex flex-row  mb-4">
-                {isVideo(file) && (
-                  <ReactPlayer
-                    url={URL.createObjectURL(file)}
-                    width={150}
-                    height={100}
-                    background-position="center"
-                    object-fit="cover"
-                    background-size="contain"
-                    controls={true}
-                  />
-                )}
-              </div>
-            ))}
-        </div>
+      {/* VIDEO */}
+      <div className="grid grid-cols-2 gap-4 w-full h-full mt-4">
+        {inputs.addFields.length > 0 &&
+          inputs.addFields.map((file, index) => (
+            <div key={index} className="mb-4">
+              {isVideo(file) && (
+                <ReactPlayer
+                  url={URL.createObjectURL(file)}
+                  width={279}
+                  height={240}
+                  className="rounded-[5px] w-[279px] h-60"
+                  controls={true}
+                />
+              )}
+            </div>
+          ))}
       </div>
     </>
   );
