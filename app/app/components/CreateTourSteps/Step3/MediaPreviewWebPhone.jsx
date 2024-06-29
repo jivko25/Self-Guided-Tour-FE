035268e11eb198a05781f17374e636f673b29e24
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-30 00:28:18 +0300

    Create MediaPreviewWebPhone.jsx

---
+++ app/app/components/CreateTourSteps/Step3/MediaPreviewWebPhone.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import ReactPlayer from "react-player";
+function MediaPreviewWebPhone({ inputs, isImage, isVideo }) {
+  return (
+    <>
+      {/* IMAGES */}
+
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="flex flex-row flex-wrap justify-between">
+          {inputs.addFields.length > 0 &&
+            inputs.addFields.map((file, index) => (
+              <div key={index} className="flex flex-row  mb-4">
+                {isImage(file) && (
+                  <Image
+                    src={URL.createObjectURL(file)}
+                    width={150}
+                    height={100}
+                    background-position="center"
+                    object-fit="cover"
+                    background-size="contain"
+                    alt="Uploaded Media"
+                  />
+                )}
+              </div>
+            ))}
+        </div>
+      </div>
+
+      {/* VIDEO  */}
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="flex  flex-row flex-wrap justify-between">
+          {inputs.addFields.length > 0 &&
+            inputs.addFields.map((file, index) => (
+              <div key={index} className="flex flex-row  mb-4">
+                {isVideo(file) && (
+                  <ReactPlayer
+                    url={URL.createObjectURL(file)}
+                    width={150}
+                    height={100}
+                    background-position="center"
+                    object-fit="cover"
+                    background-size="contain"
+                    controls={true}
+                  />
+                )}
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MediaPreviewWebPhone;
