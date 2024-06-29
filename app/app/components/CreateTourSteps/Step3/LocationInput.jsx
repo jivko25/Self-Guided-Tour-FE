77f94a61fb3b6ac8bfc09fc28f85f979fcc0724b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-30 00:28:13 +0300

    Create LocationInput.jsx

---
+++ app/app/components/CreateTourSteps/Step3/LocationInput.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+function LocationInput({ inputs, handleChange }) {
+  return (
+    <>
+    <div
+              className="flex items-center justify-start flex-col mb-[30px] w-full 
+            web:max-w-[581px] 
+            tablet:max-w-[581px] 
+            phone:max-w-[361px]"
+            >
+              <label
+                className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+                htmlFor="locationName"
+              >
+                Location Name
+              </label>
+              <input
+                id="locationName"
+                name="locationName"
+                type="text"
+                value={inputs.locationName}
+                onChange={handleChange}
+                className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px]
+                web:max-w-[581px]
+                tablet:max-w-[581px] 
+                phone:max-w-[361px]"
+                required
+              />
+            </div>
+
+            <div
+              className="flex items-center flex-col w-full mb-[20px] 
+            web:max-w-[581px] 
+            tablet:max-w-[581px] 
+            phone:max-w-[361px]"
+            >
+              <label
+                className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
+                htmlFor="locationDescription"
+              >
+                Location Description
+              </label>
+              <textarea
+                id="locationDescription"
+                name="locationDescription"
+                value={inputs.locationDescription}
+                onChange={handleChange}
+                className="bg-neutral-50 rounded border border-stone-300 w-full 
+                web:max-w-[581px] web:h-[200px] 
+                tablet:max-w-[581px] tablet:h-[200px] 
+                phone:max-w-[361px] phone:h-[178px]"
+                required
+              />
+            </div>
+    </>
+  )
+}
+
+export default LocationInput
