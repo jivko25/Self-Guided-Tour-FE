1ee6783c48c44ab1733b27bc7f80f8a1ded4ef78
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-29 15:25:18 +0300

    Update Footer.jsx

--- app/app/components/Footer.jsx
+++ app/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 // TODO
 export default function Footer() {
   return (
-    <div className="text-2xl font-bold flex justify-center phone:pb-16  text-blue-400">Footer</div>
+    <div className="text-2xl font-bold flex justify-center web:pb-0 tablet:pb-0  phone:pb-16  text-blue-400">Footer</div>
   )
 }
