c72170ea78401575b6ee664befd1df0f3d924796
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-30 16:06:16 +0300

    Update Step3.jsx

--- app/app/components/CreateTourSteps/Step3/Step3.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3.jsx
@@ -3,13 +3,13 @@ import { useCreateTour } from "@/app/context/createTourContext.jsx";
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import GoogleMapsComponent from "../../GoogleMapsComponent/GoogleMapsComponent.js";
-import LocationInput from "./LocationInput.jsx";
-import FileUpload from "./FileUpload.jsx";
-import DescriptionWeb from "./DescriptionWeb.jsx";
-import DescriptionTabletPhone from "./DescriptionTabletPhone.jsx";
-import MediaPreviewTablet from "./MediaPreviewTablet.jsx";
-import MediaPreviewWebPhone from "./MediaPreviewWebPhone.jsx";
-import NavigationButtons from "./NavigationButtons.jsx";
+import LocationInput from "./Step3Components/LocationInput.jsx";
+import FileUpload from "./Step3Components/FileUpload.jsx";
+import DescriptionWeb from "./Step3Components/DescriptionWeb.jsx";
+import DescriptionTabletPhone from "./Step3Components/DescriptionTabletPhone.jsx";
+import MediaPreviewTablet from "./Step3Components/MediaPreviewTablet.jsx";
+import MediaPreviewWebPhone from "./Step3Components/MediaPreviewWebPhone.jsx";
+import NavigationButtons from "./Step3Components/NavigationButtons.jsx";
 
 const Step3 = () => {
   const pathname = usePathname();
@@ -81,31 +81,32 @@ const Step3 = () => {
        tablet:h-full tablet:flex-col-reverse 
        phone:h-full phone:flex-col-reverse"
       >
+
+        {/* DescriptionWeb, LocationInput, FileUpload, MediaPreviewWebPhone */}
         <div
           className="flex flex-col items-center w-full
         web:justify-start web:max-h-[691px] web:overflow-y-auto web:max-w-[50%]
         tablet:w-full tablet:overflow-y-auto tablet:max-h-[350px]"
         >
-          {/* Content that appears in a web type */}
 
           <DescriptionWeb />
 
-          {/* Inputs */}
+
           <section
             className="flex items-center flex-col w-full justify-center 
           tablet:pt-[30px]
           phone:pt-[30px]"
           >
             <LocationInput inputs={inputs} handleChange={handleChange} />
-
             <FileUpload handleChange={handleChange} />
+
           </section>
 
-          {/* Images and videos for web and phone */}
+
           <section
-            className="hidden flex-col items-center justify-center w-full max-w-[581px] h-full
-          web:flex 
-          phone:flex
+            className="hidden flex-col items-center justify-center w-full web:max-w-[581px] h-full
+          web:flex web:ml-[0px]
+          phone:flex phone:ml-[30px]
           tablet:hidden"
           >
             <MediaPreviewWebPhone
@@ -116,8 +117,8 @@ const Step3 = () => {
           </section>
         </div>
 
-        {/* MAP and Description for tablet, phone*/}
-        <section
+        {/* DescriptionTabletPhone, GoogleMapsComponent */}
+        <div
           className="flex flex-col rounded-[5px] w-full 
         web:h-full web:max-w-[50%] web:max-h-[691px] 
         tablet:items-center tablet:justify-center tablet:h-screen tablet:max-w-[584px] tablet:max-h-[676px]
@@ -129,32 +130,30 @@ const Step3 = () => {
           {/* MAP */}
           <GoogleMapsComponent handleMapClick={handleMapClick} />
 
-        </section>
-      </div>
-
-      {/* Buttons and files for tablet*/}
-      <div
-        className="flex flex-col w-full h-full 
-      web:justify-start web:items-start
-      tablet:justify-center tablet:items-center  "
-      >
-        {/* Buttons */}
-        <div
-          className="flex items-start  w-full h-[138px]  bg-neutral-50 border-t border-gray-200 
-      web:w-[50%] web:justify-start web:ml-[0px] web:gap-[324px]
-      tablet:justify-center tablet:gap-[324px]
-      phone:justify-center"
-        >
-        
-          <NavigationButtons prevStep={prevStep} handleFinish={handleFinish}/>
         </div>
 
-        {/* Media files for tablet*/}
+      </div>
+
+      {/* NavigationButtons, MediaPreviewTablet */}
+      <div
+        className="flex flex-col w-full h-full 
+          web:justify-start web:items-start
+          tablet:justify-center tablet:items-center"
+      >
+        <div
+          className="flex items-start  w-full h-[138px]  bg-neutral-50 border-t border-gray-200 
+            web:w-[50%] web:justify-start web:ml-[0px] web:gap-[324px]
+            tablet:justify-center tablet:gap-[324px]
+            phone:justify-center"
+        >
+          <NavigationButtons prevStep={prevStep} handleFinish={handleFinish}/>
+
+        </div>
+
         <div
           className="hidden flex-col items-center justify-center w-full max-w-[581px] h-full
-        web:hidden 
-        tablet:flex 
-        "
+            web:hidden 
+            tablet:flex"
         >
           <MediaPreviewTablet
             inputs={inputs}
@@ -162,7 +161,9 @@ const Step3 = () => {
             isVideo={isVideo}
           />
         </div>
+        
       </div>
+
     </div>
   );
 };
