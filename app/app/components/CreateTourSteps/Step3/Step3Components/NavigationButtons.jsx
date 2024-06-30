41effc9466a6d902c15d9e759ccf89ac11f11e1b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-30 17:02:18 +0300

    Update NavigationButtons.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
@@ -1,10 +1,15 @@
-"use client"
+"use client";
 import React from "react";
 import Btn from "../../../Buttons/Btn.jsx";
 
 function NavigationButtons({ prevStep, handleFinish }) {
   return (
-    <>
+    <div
+      className="flex items-start  w-full h-[138px]  bg-neutral-50 border-t border-gray-200 
+            web:w-[50%] web:justify-start web:ml-[0px] web:gap-[324px]
+            tablet:justify-center tablet:gap-[324px]
+            phone:justify-center"
+    >
       <Btn
         variant="transparent"
         className="hidden tablet:inline-flex web:inline-flex text-center text-gray-900 text-base font-semibold px-4 py-3 w-32 h-[43px] justify-center items-center border-b-2 border-neutral-50"
@@ -17,7 +22,7 @@ function NavigationButtons({ prevStep, handleFinish }) {
         text="Next"
         onClick={handleFinish}
       />
-    </>
+    </div>
   );
 }
 
