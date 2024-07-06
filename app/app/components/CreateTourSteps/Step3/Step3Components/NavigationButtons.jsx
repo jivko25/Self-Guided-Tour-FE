cc4930388b47b1783455c932313acdbc896f84b2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-06 17:41:10 +0300

    Update NavigationButtons.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
@@ -8,11 +8,14 @@ function NavigationButtons({ prevStep, handleFinish }) {
       className="flex items-start w-full h-[128px] pt-[20px]
             web:justify-center web:max-w-[50%] web:gap-[380px] web:bg-neutral-50 web:border-t web:border-gray-200
             tablet:justify-center tablet:gap-[350px] tablet:bg-neutral-50 tablet:border-t tablet:border-gray-200
-            phone:justify-center mb-[30px]"
+            phone:justify-center phone:mb-[50px] phone:pt-[20px]
+            smallPhone:justify-center smallPhone:pt-[0px]"
     >
       <Btn
         variant="transparent"
-        className="hidden tablet:inline-flex web:inline-flex text-center text-gray-900 text-base font-semibold px-4 py-3 w-[128px] h-[43px] justify-center items-center border-b-2 border-neutral-50 "
+        className="hidden text-center text-gray-900 text-base font-semibold px-4 py-3 w-[128px] h-[43px] justify-center items-center border-b-2 border-neutral-50
+        web:inline-flex  
+        tablet:inline-flex"
         text="Prev"
         onClick={prevStep}
       />
@@ -21,7 +24,8 @@ function NavigationButtons({ prevStep, handleFinish }) {
         className="text-center text-gray-900 text-base font-semibold justify-center items-center flex border-b-2 border-yellow-500 h-[43px]
         web:px-[16px] web:py-[12px] web:w-[128px]
         tablet:px-[16px] tablet:py-[12px] tablet:w-[128px]
-        phone:px-[16px] phone:py-[12px] phone:w-[177px] "
+        phone:px-[16px] phone:py-[12px] phone:w-[177px] 
+        smallPhone:px-[16px] smallPhone:py-[12px] smallPhone:w-[130px]"
         text="Next"
         onClick={handleFinish}
       />
