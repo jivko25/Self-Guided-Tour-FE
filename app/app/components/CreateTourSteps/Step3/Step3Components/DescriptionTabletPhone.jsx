78137bbbca48e7604e88f90dd1f780ac5649c544
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-06 17:40:57 +0300

    Update DescriptionTabletPhone.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/DescriptionTabletPhone.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/DescriptionTabletPhone.jsx
@@ -5,22 +5,25 @@ function DescriptionTabletPhone() {
   return (
     <section
       className="hidden items-center justify-between w-full 
-          web:hidden web:max-w-[581px] web:pt-[10px] web:pb-[20px]
+          web:hidden 
           tablet:flex tablet:max-w-[581px] tablet:pt-[10px] tablet:pb-[20px] tablet:items-start
-          phone:flex phone:max-w-[361px] phone:pt-[10px] phone:pb-[20px] phone:text-sm phone:items-start"
+          phone:flex phone:max-w-[361px] phone:pt-[10px] phone:pb-[20px] phone:text-sm phone:items-start
+          smallPhone:flex smallPhone:max-w-[290px] smallPhone:pt-[10px] smallPhone:pb-[20px] smallPhone:items-start"
     >
       <div>
         <h4
           className="text-gray-900 text-2xl font-medium leading-9 
               tablet:font-medium
-              phone:text-xl "
+              phone:text-xl 
+              smallPhone:text-base"
         >
           Describe and shoot
         </h4>
         <p
           className="w-[425px] text-blue-950 text-base font-normal leading-normal 
                 tablet:w-[425px] tablet:text-base
-                phone:w-[269px] phone:text-sm"
+                phone:w-[269px] phone:text-sm
+                smallPhone:w-[190px] smallPhone:text-sm"
         >
           Add description, images and audio files for the location so that the
           other travelers know more about it.
@@ -28,8 +31,9 @@ function DescriptionTabletPhone() {
       </div>
       <p
         className="text-yellow-500 text-base font-medium leading-normal 
-              tablet:w-[85px] tablet:text-base
-              phone:w-[74px] phone:text-sm"
+              tablet:w-[85px] tablet:text-base tablet:mt-[10px]
+              phone:w-[74px] phone:text-sm phone:mt-[10px]
+              smallPhone:text-sm smallPhone:mt-[10px]"
       >
         Step 3 of 4
       </p>
