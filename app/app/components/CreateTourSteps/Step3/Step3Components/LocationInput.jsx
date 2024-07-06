902cf9a287efd73d615197a20411cb7a2739c88f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-06 15:10:01 +0300

    Update LocationInput.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/LocationInput.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/LocationInput.jsx
@@ -22,7 +22,7 @@ function LocationInput({ inputs, handleChange }) {
           type="text"
           value={inputs.locationName}
           onChange={handleChange}
-          className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px]
+          className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px] pl-[10px]
                 web:max-w-[581px]
                 tablet:max-w-[581px] 
                 phone:max-w-[361px]"
@@ -48,7 +48,7 @@ function LocationInput({ inputs, handleChange }) {
           type="text"
           value={inputs.locationCity}
           onChange={handleChange}
-          className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px]
+          className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px] max-w-[288px] pl-[10px]
                 web:max-w-[581px]
                 tablet:max-w-[581px] 
                 phone:max-w-[361px]"
@@ -73,7 +73,7 @@ function LocationInput({ inputs, handleChange }) {
           name="locationDescription"
           value={inputs.locationDescription}
           onChange={handleChange}
-          className="bg-neutral-50 rounded border border-stone-300 w-full 
+          className="bg-neutral-50 rounded border border-stone-300 w-full pl-[10px]
                 web:max-w-[581px] web:h-[200px] 
                 tablet:max-w-[581px] tablet:h-[200px] 
                 phone:max-w-[361px] phone:h-[178px]"
