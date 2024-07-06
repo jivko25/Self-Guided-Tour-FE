c509141ccdcd8817873e798d95e8976925bf0285
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-06 17:41:06 +0300

    Update LocationInput.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/LocationInput.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/LocationInput.jsx
@@ -5,10 +5,11 @@ function LocationInput({ inputs, handleChange }) {
   return (
     <>
       <div
-        className="flex items-center justify-start flex-col mb-[30px] w-full 
+        className="flex items-center justify-start flex-col mb-[30px] w-full
             web:max-w-[581px] 
             tablet:max-w-[581px] 
-            phone:max-w-[361px]"
+            phone:max-w-[361px]
+            smallPhone:max-w-[288px]"
       >
         <label
           className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
@@ -25,7 +26,8 @@ function LocationInput({ inputs, handleChange }) {
           className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px] pl-[10px]
                 web:max-w-[581px]
                 tablet:max-w-[581px] 
-                phone:max-w-[361px]"
+                phone:max-w-[361px]
+                smallPhone:max-w-[288px]"
           required
         />
       </div>
@@ -34,7 +36,8 @@ function LocationInput({ inputs, handleChange }) {
         className="flex items-center justify-start flex-col mb-[30px] w-full 
             web:max-w-[581px] 
             tablet:max-w-[581px] 
-            phone:max-w-[361px]"
+            phone:max-w-[361px]
+            smallPhone:max-w-[288px]"
       >
         <label
           className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
@@ -51,7 +54,8 @@ function LocationInput({ inputs, handleChange }) {
           className="bg-neutral-50 rounded border border-stone-300 w-full h-[60px] max-w-[288px] pl-[10px]
                 web:max-w-[581px]
                 tablet:max-w-[581px] 
-                phone:max-w-[361px]"
+                phone:max-w-[361px]
+                smallPhone:max-w-[288px]"
           required
         />
       </div>
@@ -60,7 +64,8 @@ function LocationInput({ inputs, handleChange }) {
         className="flex items-center flex-col w-full mb-[20px] 
             web:max-w-[581px] 
             tablet:max-w-[581px] 
-            phone:max-w-[361px]"
+            phone:max-w-[361px]
+            smallPhone:max-w-[288px]"
       >
         <label
           className="flex justify-start w-full text-gray-900 text-base font-medium leading-normal"
@@ -76,7 +81,8 @@ function LocationInput({ inputs, handleChange }) {
           className="bg-neutral-50 rounded border border-stone-300 w-full pl-[10px]
                 web:max-w-[581px] web:h-[200px] 
                 tablet:max-w-[581px] tablet:h-[200px] 
-                phone:max-w-[361px] phone:h-[178px]"
+                phone:max-w-[361px] phone:h-[178px]
+                smallPhone:max-w-[288px] smallPhone:h-[120px]"
           required
         />
       </div>
