186a7488d20eac8ea39d0f0cb22b204cfdc66da9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-06 17:41:08 +0300

    Update MediaPreviewWebPhone.jsx

--- app/app/components/CreateTourSteps/Step3/Step3Components/MediaPreviewWebPhone.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/MediaPreviewWebPhone.jsx
@@ -11,6 +11,7 @@ function MediaPreviewWebPhone({ inputs, isImage, isVideo }) {
         className="
       web:grid web:grid-cols-2 web:gap-4 web:w-full web:h-full web:pl-[0px] web:overflow-hidden web:mr-[0px]
       phone:flex  phone:mr-[20px] phone:w-full phone:pl-[20px] phone:overflow-x-auto
+      smallPhone:flex  smallPhone:mr-[20px] smallPhone:w-full smallPhone:pl-[20px] smallPhone:overflow-x-auto
       "
       
       >
@@ -20,7 +21,7 @@ function MediaPreviewWebPhone({ inputs, isImage, isVideo }) {
               key={index}
               className={`${
                 isImage(file) && index === 0 ? "web:col-span-2" : ""
-              } mb-1 phone:flex-shrink-0 phone:mr-[10px] web:mr-[0px] `}
+              } mb-1 phone:flex-shrink-0 phone:mr-[10px] smallPhone:flex-shrink-0 smallPhone:mr-[10px] web:mr-[0px] `}
             >
               {isImage(file) && (
                 <Image
@@ -31,7 +32,7 @@ function MediaPreviewWebPhone({ inputs, isImage, isVideo }) {
                     index === 0
                       ? "web:w-full web:h-60"
                       : "web:w-[279px] web:h-60"
-                  } phone:w-[269px] phone:h-60 rounded-[5px] object-cover`}
+                  } phone:w-[269px] phone:h-60 smallPhone:w-[269px] smallPhone:h-[150px] rounded-[5px] object-cover`}
                   alt="Uploaded Media"
                 />
               )}
@@ -40,7 +41,7 @@ function MediaPreviewWebPhone({ inputs, isImage, isVideo }) {
                   url={URL.createObjectURL(file)}
                   width={269}
                   height={240}
-                  className="web:rounded-[5px] web:w-[279px] web:h-60 phone:w-[269px] phone:h-60 phone:rounded-[5px]"
+                  className="web:rounded-[5px] web:w-[279px] web:h-60 phone:w-[269px] phone:h-60 phone:rounded-[5px] smallPhone:w-[269px] smallPhone:h-[150px] smallPhone:rounded-[5px]"
                   controls={true}
                 />
               )}
