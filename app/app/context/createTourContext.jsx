a20938bf1a1383c34bbe256ef704884ab493539d
ajummer <makeudreamempty@gmail.com>
2024-07-07 13:29:54 +0300

    feat: save steps data in localstorage

--- app/app/context/createTourContext.jsx
+++ app/app/context/createTourContext.jsx
@@ -1,27 +1,53 @@
-import { createContext, useContext, useState } from "react";
+"use client";
+import { createContext, useContext, useState, useEffect } from "react";
+
+const LOCAL_STORAGE_KEY = "savedTourFormData";
 
 const CreateTourContext = createContext();
 
 export const CreateTourProvider = ({ children }) => {
-  const [step, setStep] = useState(0);
-  const [formData, setFormData] = useState({
-    step1Data: {
-      tour: "",
-      destination: "",
-      duration: "",
-      price: "",
-    },
-    step2Data: [],
-    step3Data: "",
-    step4Data: {
-      summary: "",
-      thumbnailImage: null,
-    },
-  });
+  const loadInitialState = () => {
+    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return savedData
+      ? JSON.parse(savedData)
+      : {
+          step1Data: {
+            tour: "",
+            destination: "",
+            duration: "",
+            price: "",
+          },
+          step2Data: [],
+          step3Data: "",
+          step4Data: {
+            summary: "",
+            thumbnailImage: null,
+          },
+        };
+  };
 
-  console.log(formData.step4Data);
+  const [step, setStep] = useState(0);
+  const [formData, setFormData] = useState(loadInitialState());
+
+  // Currently it saves everytime formData changes , but it's a better idea to save it with a Save Draft Button
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  }, [formData]);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedData) {
+      const userConfirmed = window.confirm(
+        "Do you want to load the saved draft?"
+      );
+      if (userConfirmed) {
+        setFormData(JSON.parse(savedData));
+      }
+    }
+  }, []);
 
   console.log(formData);
+
   const nextStep = () => setStep((prevStep) => prevStep + 1);
   const prevStep = () => setStep((prevStep) => prevStep - 1);
   const goToStep = (stepIndex) => setStep(stepIndex);
