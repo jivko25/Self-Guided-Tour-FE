184778d777e8aba5b79b556f571c6e21d3577276
ajummer <makeudreamempty@gmail.com>
2024-07-08 12:48:44 +0300

    fix: add emptyFormData as object

--- app/app/context/createTourContext.jsx
+++ app/app/context/createTourContext.jsx
@@ -1,48 +1,61 @@
 "use client";
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, useRef } from "react";
 
 const LOCAL_STORAGE_KEY = "savedTourFormData";
 
 const CreateTourContext = createContext();
 
 export const CreateTourProvider = ({ children }) => {
+  const emptyFormData = {
+    step1Data: {
+      tour: "",
+      destination: "",
+      duration: "",
+      price: "",
+    },
+    step2Data: [],
+    step3Data: "",
+    step4Data: {
+      summary: "",
+      thumbnailImage: null,
+    },
+  };
+
   const loadInitialState = () => {
-    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    return savedData
-      ? JSON.parse(savedData)
-      : {
-          step1Data: {
-            tour: "",
-            destination: "",
-            duration: "",
-            price: "",
-          },
-          step2Data: [],
-          step3Data: "",
-          step4Data: {
-            summary: "",
-            thumbnailImage: null,
-          },
-        };
+    if (typeof window !== "undefined") {
+      // Check if the code is running in the browser
+      const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+      return savedData ? JSON.parse(savedData) : emptyFormData;
+    }
+    return emptyFormData; // Return empty form data during SSR
   };
 
   const [step, setStep] = useState(0);
   const [formData, setFormData] = useState(loadInitialState());
+  const hasPrompted = useRef(false); // Use a ref to ensure the prompt only happens once
 
   // Currently it saves everytime formData changes , but it's a better idea to save it with a Save Draft Button
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+    if (hasPrompted.current) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+    }
   }, [formData]);
 
   useEffect(() => {
-    const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (savedData) {
-      const userConfirmed = window.confirm(
-        "Do you want to load the saved draft?"
-      );
-      if (userConfirmed) {
-        setFormData(JSON.parse(savedData));
+    if (!hasPrompted.current) {
+      const savedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (savedData) {
+        const userConfirmed = window.confirm(
+          "Do you want to load the saved draft?"
+        );
+        console.log(userConfirmed);
+        if (userConfirmed) {
+          setFormData(JSON.parse(savedData));
+        } else {
+          setFormData(emptyFormData);
+        }
       }
+      hasPrompted.current = true; // Ensure this runs only once
     }
   }, []);
 
