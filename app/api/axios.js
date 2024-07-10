acb7aba28df058ae38064fc5edbeb01000f22e15
ajummer <makeudreamempty@gmail.com>
2024-07-10 14:50:00 +0300

    fix: update node env with production to test prod server

--- app/api/axios.js
+++ app/api/axios.js
@@ -1,11 +1,16 @@
 import Axios from "axios";
 import https from "https";
 //TODO: Get the base url from the env
-const BASE_URL = (process.env.NODE_ENV === "production") ? `${process.env.NEXT_PUBLIC_BASE_URL}/api` : "https://localhost:7038/api";
+const BASE_URL =
+  process.env.NEXT_PUBLIC_NODE_ENV === "production"
+    ? `${process.env.NEXT_PUBLIC_BASE_URL}/api`
+    : "https://localhost:7038/api";
+
+console.log(BASE_URL);
 
 // to be removed for production
 const agent = new https.Agent({
-  rejectUnauthorized: (process.env.NODE_ENV === 'production') ? true : false,
+  rejectUnauthorized: process.env.NODE_ENV === "production" ? true : false,
 });
 
 export const axios = Axios.create({
