ea5e22a23ad7a5ef3f7ccaf72ec128cd8afb39c0
ajummer <makeudreamempty@gmail.com>
2024-07-13 22:54:03 +0300

    fix: changed setCookie httpOnly to false

--- app/app/utils/authHelper.js
+++ app/app/utils/authHelper.js
@@ -1,29 +1,29 @@
-"server-only"
+"server-only";
 
-import { cookies } from "next/headers"
+import { cookies } from "next/headers";
 import { decodeToken, generateToken } from "./jwt";
 
 export function setCookie(name, data) {
-    const jwt = generateToken(data);
-    cookies().set(name, jwt, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 24 * 30,
-        sameSite: 'Strict',
-      });
+  const jwt = generateToken(data);
+  cookies().set(name, jwt, {
+    httpOnly: false,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 30,
+    sameSite: "Strict",
+  });
 }
 
 export function getCookie(name) {
-    const cookie = cookies().get(name);
-    return cookie ? decodeToken(cookie.value) : null;
+  const cookie = cookies().get(name);
+  return cookie ? decodeToken(cookie.value) : null;
 }
 
 export function deleteCookie(name) {
-    cookies().set(name, '', {
+  cookies().set(name, "", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: process.env.NODE_ENV === "production",
     maxAge: -1,
-    sameSite: 'strict',
-    path: '/',
+    sameSite: "strict",
+    path: "/",
   });
 }
