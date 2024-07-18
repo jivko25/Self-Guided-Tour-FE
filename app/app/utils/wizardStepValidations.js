1fba53f966c2e22375fc93d63fb464bbfdbc3ed0
ajummer <makeudreamempty@gmail.com>
2024-07-18 14:56:55 +0300

    feat: add validations util

--- app/app/utils/wizardStepValidations.js
+++ app/app/utils/wizardStepValidations.js
@@ -1,4 +1,17 @@
-export const validateStep = (step, formData) => {
+export const validateStep = (step, formData, targetStep) => {
+  console.log(formData);
+  const isObjectFilled = (obj) => {
+    for (let key in obj) {
+      if (obj[key] === null || obj[key] === undefined || obj[key] === "") {
+        return false;
+      }
+      if (Array.isArray(obj[key]) && obj[key].length === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
   switch (step) {
     case 0:
       return (
@@ -8,7 +21,27 @@ export const validateStep = (step, formData) => {
         formData.step1Data.price
       );
     case 1:
-      return formData.step2Data.length > 0;
+    case 1:
+      // Only validate the full details if the target step is 4
+      if (targetStep === 3) {
+        return (
+          formData.step2Data.length > 0 &&
+          formData.step2Data.every((loc) =>
+            isObjectFilled({
+              location: loc.location,
+              placeId: loc.placeId,
+              locationCity: loc.locationCity,
+              locationDescription: loc.locationDescription,
+              latitude: loc.latitude,
+              longitude: loc.longitude,
+              addFields: loc.addFields,
+            })
+          )
+        );
+      } else {
+        // Otherwise, just check if step2Data is not empty
+        return formData.step2Data.length > 0;
+      }
     case 2:
       return (
         formData.step3Data.locationName &&
@@ -23,7 +56,7 @@ export const validateStep = (step, formData) => {
 
 export const canProceedToStep = (targetStep, formData) => {
   for (let i = 0; i < targetStep; i++) {
-    if (!validateStep(i, formData)) {
+    if (!validateStep(i, formData, targetStep) && i !== 3) {
       return false;
     }
   }
