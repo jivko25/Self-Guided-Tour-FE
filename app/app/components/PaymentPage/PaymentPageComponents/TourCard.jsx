e4a1b3e6130950d41357336ba9d963e7b88c317a
Михаел Йорданов <gaws@mail.bg>
2024-07-29 22:53:15 +0300

    structure card

---
+++ app/app/components/PaymentPage/PaymentPageComponents/TourCard.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import StarRating from "../../StarRating/StarRating";
+import IconsBar from "./IconsBar";
+
+const tour = {
+  title: "Sofia theaters",
+  description: "Sofia theaters",
+  location: "Sofia, Bulgaria",
+  duration: 90,
+  price: 9.99,
+  type: "Walking tour",
+};
+function TourCard() {
+  return (
+    <section className="flex flex-col gap-5 rounded-xl border border-tourCard">
+      <Image
+        className="rounded-t-xl"
+        src="https://selfguidedstorage.blob.core.windows.net/landmark-resources/ec1c648c-d033-41f4-a73e-abe0046a15f2.jpg"
+        alt={tour.title}
+        width={582}
+        height={161}
+      />
+      <div className="m-5">
+        <div className=" flex flex-row justify-between">
+          <h1>{tour.title}</h1>
+          <StarRating maxRating={1} />
+        </div>
+        <div>
+          <IconsBar
+            tourType={tour.type}
+            duration={tour.duration}
+            location={tour.location}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TourCard;
