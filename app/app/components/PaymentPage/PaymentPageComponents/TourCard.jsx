fdc8690dff2fcd47aefee380d9781926077f83bf
Михаел Йорданов <gaws@mail.bg>
2024-07-30 11:26:15 +0300

    chore: Refactor TourCard component structure and styles

--- app/app/components/PaymentPage/PaymentPageComponents/TourCard.jsx
+++ app/app/components/PaymentPage/PaymentPageComponents/TourCard.jsx
@@ -14,32 +14,32 @@ const tour = {
 };
 function TourCard() {
   return (
-    <section className="flex flex-col gap-5 rounded-xl border border-tourCard">
+    <section className="flex flex-col justify-between  rounded-xl border border-tourCard ">
       <Image
-        className="rounded-t-xl"
+        className="rounded-t-xl h-[161px] object-center object-cover"
         src="https://selfguidedstorage.blob.core.windows.net/landmark-resources/ec1c648c-d033-41f4-a73e-abe0046a15f2.jpg"
         alt={tour.title}
         width={582}
         height={161}
       />
       <div className="m-5">
-        <div className=" flex flex-row justify-between">
+        <div className=" flex flex-row justify-between text-[24px] m-2">
           <h1>{tour.title}</h1>
-          <StarRating maxRating={1} />
+          <StarRating rating={4.6} />
         </div>
-        <div>
-          <IconsBar
-            tourType={tour.type}
-            duration={tour.duration}
-            location={tour.location}
-          />
-        </div>
-        <div className="flex justify-between m-6">
+
+        <IconsBar
+          tourType={tour.type}
+          duration={tour.duration}
+          location={tour.location}
+        />
+
+        <div className="flex justify-between m-6 text-[24px]">
           <p>Total</p>
           <h2>USD {tour.price}</h2>
         </div>
         <div className=" text-center">
-          <Button text="Confirm and pay" />
+          <Button variant="primary-very-long" text="Confirm and pay" />
         </div>
       </div>
     </section>
