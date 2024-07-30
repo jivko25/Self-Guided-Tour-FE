adee74821baf0003227f911d55e443b5dcd7a51e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-30 11:24:21 +0300

    Update page.jsx

--- app/app/tour/[id]/page.jsx
+++ app/app/tour/[id]/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Star from "@/app/components/Svg/Star";
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 
@@ -8,15 +9,16 @@ function TourDetails() {
   return (
     <div className="flex flex-col items-center w-full">
       <div className="flex flex-col w-[65%] justify-center items-center">
-        
-        <div className="flex justify-start items-center w-full">
+        <div className="flex justify-start items-center w-full gap-[30px]">
           <h1 className="text-[#081120] text-[39px] font-medium font-['Inter'] leading-[58.50px]">
             Sofia Theaters
           </h1>
-          <img src="" alt="" />
-          <span className="text-[#13294b] text-base font-normal font-['Inter'] leading-none">
-            4.8
-          </span>
+          <div className="flex gap-[10px]">
+            <Star />
+            <span className="text-[#13294b] text-base font-normal font-['Inter'] leading-none">
+              4.8
+            </span>
+          </div>
         </div>
 
         <div className="grid grid-cols-4 grid-rows-2  gap-4 w-full  h-full max-h-[520px] items-center justify-center">
@@ -46,31 +48,40 @@ function TourDetails() {
             alt="Image 3"
           />
         </div>
-        
       </div>
 
-      <div className="flex items-center justify-evenly w-full h-[200px]">
-        <div>
+      <div className="flex items-center justify-around w-[65%] h-[250px] border-b-2  border-[#d1d0d8]">
+        <div className="">
           <img src="" alt="" />
           <div>
-            <h4>Title</h4>
-            <p>Lorem</p>
+            <h2 className="text-[#081120] text-xl font-medium font-['Inter']">
+              This is a 2.5km walking tour
+            </h2>
+            <p className="text-[#13294b] text-base font-normal font-['Inter']">
+              To complete it you will need average of 90min.
+            </p>
           </div>
         </div>
         <div>
           <img src="" alt="" />
           <div>
-            <h4>Title</h4>
-            <p>Lorem</p>
+            <h2 className="text-[#081120] text-xl font-medium font-['Inter']">
+              This tour is located in Sofia
+            </h2>
+            <p className="text-[#13294b] text-base font-normal font-['Inter']">
+              Sofia is the capital of Bulgaria
+            </p>
           </div>
         </div>
       </div>
 
       <div className="flex items-center justify-evenly w-full h-[500px]">
-        <div className="w-[733px]">
+        <div className="flex flex-col gap-[50px] w-[733px]">
           <div className="">
-            <h3>Title</h3>
-            <p>
+            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9">
+              About the tour
+            </h3>
+            <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
               atque tenetur quos in esse ipsam eos officiis, asperiores facilis
               ad alias sint quod mollitia doloribus ipsa facere quas sunt
@@ -78,8 +89,10 @@ function TourDetails() {
             </p>
           </div>
           <div className="">
-            <h3>Title</h3>
-            <p>
+            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9">
+              Directions to starting point
+            </h3>
+            <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
               atque tenetur quos in esse ipsam eos officiis, asperiores facilis
               ad alias sint quod mollitia doloribus ipsa facere quas sunt
@@ -90,19 +103,30 @@ function TourDetails() {
 
         <div className="w-[430px]">
           <div>
-            <h2>Title</h2>
-            <h1>STARS</h1>
+            <h2 className="text-[#081120] text-[31px] font-medium font-['Inter']">
+              Sofia Theaters Tour
+            </h2>
+            <div className="flex">
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+            </div>
           </div>
 
           <div>
-            <h2>Title</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem,
-              magni consectetur cum repellendus, sapiente reiciendis dolor
-              eveniet inventore tempore temporibus ullam. Rem vitae, sed omnis
-              earum dicta beatae nihil expedita?
+            <h2 className="text-[#081120] text-2xl font-medium font-['Inter']">
+              USD 9.99
+            </h2>
+            <p className="w-[430px] text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
+              Explore the beautiful buildings and green parks of Sofia’s biggest
+              theaters. Take a walk trough a beautiful sightseeing in the
+              historical center of the city.
             </p>
-            <button>Buy Tour</button>
+            <button className="rounded-[5px] px-4 py-3 bg-[#13294b] w-[430px] h-11 text-center text-white text-base font-semibold font-['Inter']">
+              Buy Tour
+            </button>
           </div>
         </div>
       </div>
