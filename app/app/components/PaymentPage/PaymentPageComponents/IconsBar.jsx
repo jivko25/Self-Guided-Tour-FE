bd0ae1f1370f4236c893b35acea8324bd27fd49d
Михаел Йорданов <gaws@mail.bg>
2024-07-30 11:25:08 +0300

    adjust border color

--- app/app/components/PaymentPage/PaymentPageComponents/IconsBar.jsx
+++ app/app/components/PaymentPage/PaymentPageComponents/IconsBar.jsx
@@ -4,16 +4,19 @@ import Time from "../../../public/svg/time.svg";
 import Location from "../../../public/svg/location-sharp.svg";
 function IconsBar({ tourType, duration, location }) {
   return (
-    <ul className="flex justify-between border-b pb-6  border-b-tourCard">
-      <li className="flex justify-center items-center gap-3">
+    <ul
+      style={{ borderColor: "#D1D0D8" }}
+      className="flex justify-between border-b pb-6  border-b-tourCard  m-auto"
+    >
+      <li className="flex justify-center items-center gap-1">
         <Image src={Walk} />
         <p>{tourType}</p>
       </li>
-      <li className="flex justify-center items-center gap-3">
+      <li className="flex justify-center items-center gap-1">
         <Image src={Time} />
         <p>Average of {duration} minutes</p>
       </li>
-      <li className="flex justify-center items-center gap-3">
+      <li className="flex justify-center items-center gap-1">
         <Image src={Location} />
         <p>{location}</p>
       </li>
