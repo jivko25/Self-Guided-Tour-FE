be07d343c5d0c077bc1a8e7ae77aec4990ed1bf7
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-08-01 21:58:13 +0300

    fix to create tour from android

--- app/app/context/createTourContext.jsx
+++ app/app/context/createTourContext.jsx
@@ -254,7 +254,9 @@ export const CreateTourProvider = ({ children }) => {
     let error = null;
 
     try {
-      const response = await axiosTour.post("/create-tour", formData);
+      const response = await axiosTour.post("/create-tour", formData, {
+        withCredentials: false,
+      });
       
       data = response.data;
     } catch (err) {
