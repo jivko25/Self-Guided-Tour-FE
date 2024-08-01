96e75eb4bb7d7a5fe33508bc35348f866d49be1c
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-08-01 20:22:36 +0300

    changed url to development and added logout btn in modile header menu

--- app/app/components/MobileMenuOverlay/MobileMenuOverlayUser.jsx
+++ app/app/components/MobileMenuOverlay/MobileMenuOverlayUser.jsx
@@ -1,5 +1,10 @@
 import Btn from "../Buttons/Btn.jsx";
-export default function MobileMenuOverlayUser({menuOverlayVisible}) {
+export default function MobileMenuOverlayUser({menuOverlayVisible, handleLogout}) {
+  const logout = () => {
+    if (handleLogout) {
+      handleLogout();
+    }
+  }
   return (
     <div className="flex flex-col justify-center items-center gap-4  mt-8" onClick={menuOverlayVisible}>
       <Btn type="button" variant="transparent" text="Explore" link="/explore" onClick={menuOverlayVisible}/>
@@ -10,6 +15,12 @@ export default function MobileMenuOverlayUser({menuOverlayVisible}) {
         text="My Tours"
         link="/my-tours"
       />
+            <Btn
+                variant="secondary-outlined"
+                text="Logout"
+                type={"submit"}
+                onClick={logout}
+              />
       <div className="w-11/12 flex flex-col gap-2 my-8">
         <Btn fullWidth text="Menu" link="/menu" onClick={menuOverlayVisible}/>
       </div>
