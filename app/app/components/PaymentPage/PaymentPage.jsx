a24ade51809605a1c1132b21132547319faca7aa
Михаел Йорданов <gaws@mail.bg>
2024-08-02 12:53:51 +0300

    finish up all screen styles

--- app/app/components/PaymentPage/PaymentPage.jsx
+++ app/app/components/PaymentPage/PaymentPage.jsx
@@ -2,12 +2,21 @@ import Btn from "../Buttons/Btn";
 import PaymentForm from "./PaymentPageComponents/PaymentForm";
 import TourCard from "./PaymentPageComponents/TourCard";
 
+import XButton from "../Buttons/XButton";
 function PaymentPage() {
   return (
-    <section className="m-[215px] mt-16 text-[#081120]">
-      <div className="flex justify-between mb-16 ">
-        <h1 className="text-[39px] font-medium">Payment Information</h1>
-        <Btn variant="transperant" text="X Cancel" className="flex" />
+    <section
+      className="m-[215px] text-[#081120]
+                       web:mt-16  web:mb-[136px]
+                       tablet:mt-[125px]
+                       phone:mt-[92px]
+                        "
+    >
+      <div className="flex justify-between web:mb-[92px] tablet:mb-16 phone:mb-16">
+        <h1 className="web:text-[39px] tablet:text-[31px] phone:text-[25px]">
+          Payment Information
+        </h1>
+        <XButton />
       </div>
       <div className="flex  gap-16  justify-center">
         <PaymentForm />
--- app/app/components/PaymentPage/PaymentPageComponents/PaymentForm.jsx
+++ app/app/components/PaymentPage/PaymentPageComponents/PaymentForm.jsx
@@ -8,40 +8,50 @@ function PaymentForm() {
     <form
       className="flex flex-col
             web:w-[733px]
-            tablet:w-[583px]"
+            tablet:w-[583px]
+            phone:w-[361px]"
     >
       <InputField
-        classes="mb-[64px]"
+        classes="mb-[64px] phone:mb-[61px]"
         label="Your Name"
         placeholder="Your Name"
       />
       <InputField
-        classes="mb-[120px]"
+        classes="web:mb-[120px]
+                tablet:mb-[92px]
+                phone:mb-[89px]"
         label="Email Address"
         placeholder="Email"
       />
-      <div className="mb-[121px]">
+      <div className="mb-[121px] tablet:mb-28 phone:mb-[89px]">
         <InputField label="Card Number" placeholder="Card Number" />
         <PaymentIcons className="mt-4 ml-9" />
       </div>
-      <div className="flex gap-6 mb-[64px]">
+      <div className="flex gap-6 mb-[64px] phone:mb-[61px]">
         <InputField
           label="Expiration Date"
           placeholder="MM/YY"
           classes="w-[354px]"
         />
-        <InputField label="CVV" placeholder="CVV" classes="w-[354px]" />
+        <InputField label="CVC" placeholder="CVc" classes="w-[354px]" />
       </div>
-      <div className="flex gap-6">
+      <InputField label="Country" placeholder="Country" classes="mb-[64px]" />
+      <div className="flex flex-col tablet:flex-row gap-6">
         <Btn
           variant="transparent-outlined"
-          className="web:w-[279px]"
+          className=" web:w-[279px]
+                    tablet:w-[282px]
+                    phone:w-[361px]
+                    phone:order-2
+                    "
           text="Cancel"
         />
         <Btn
           variant="filled"
           text="Confirm and Pay"
-          className="web:w-[430px]"
+          className="web:w-[430px]
+                    tablet:w-[282px] tablet:order-2
+                    phone:w-[361px]"
         />
       </div>
     </form>
