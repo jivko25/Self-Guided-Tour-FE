6fa8bf17eb3750f89c162b60b27d8f3a372d7249
Михаел Йорданов <gaws@mail.bg>
2024-08-03 23:28:03 +0300

    wrap payment page i context

--- app/app/components/PaymentPage/PaymentPage.jsx
+++ app/app/components/PaymentPage/PaymentPage.jsx
@@ -1,28 +1,36 @@
-import Btn from "../Buttons/Btn";
+"use client";
 import PaymentForm from "./PaymentPageComponents/PaymentForm";
 import TourCard from "./PaymentPageComponents/TourCard";
+import { useRouter } from "next/navigation";
 
-import XButton from "../Buttons/XButton";
+import CloseButton from "../Buttons/CloseButton";
+import { PaymentProvider } from "@/app/context/paymentContext";
 function PaymentPage() {
+  const router = useRouter();
+  function handleClose() {
+    router.back();
+  }
   return (
-    <section
-      className="m-[215px] text-[#081120]
-                       web:mt-16  web:mb-[136px]
-                       tablet:mt-[125px]
-                       phone:mt-[92px]
-                        "
-    >
-      <div className="flex justify-between web:mb-[92px] tablet:mb-16 phone:mb-16">
-        <h1 className="web:text-[39px] tablet:text-[31px] phone:text-[25px]">
-          Payment Information
-        </h1>
-        <XButton />
-      </div>
-      <div className="flex  gap-16  justify-center">
-        <PaymentForm />
-        <TourCard />
-      </div>
-    </section>
+    <PaymentProvider>
+      <section
+        className="m-[215px] text-[#081120]
+      web:mt-16  web:mb-[136px]
+      tablet:mt-[125px]
+      phone:mt-[92px]
+      "
+      >
+        <div className="flex justify-between web:mb-[92px] tablet:mb-16 phone:mb-16">
+          <h1 className="web:text-[39px] tablet:text-[31px] phone:text-[25px]">
+            Payment Information
+          </h1>
+          <CloseButton onClick={handleClose} />
+        </div>
+        <div className="flex  gap-16  justify-center">
+          <PaymentForm onClose={handleClose} />
+          <TourCard />
+        </div>
+      </section>
+    </PaymentProvider>
   );
 }
 
