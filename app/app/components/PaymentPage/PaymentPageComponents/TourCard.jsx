72713dc332ffa4e23aacec5c037584a5f2aeb169
Михаел Йорданов <gaws@mail.bg>
2024-08-03 23:27:45 +0300

    get tour from API instead of using hardcoded one

--- app/app/components/PaymentPage/PaymentPageComponents/TourCard.jsx
+++ app/app/components/PaymentPage/PaymentPageComponents/TourCard.jsx
@@ -3,16 +3,29 @@ import Image from "next/image";
 import StarRating from "../../StarRating/StarRating";
 import IconsBar from "./Card/IconsBar";
 import Button from "../../Buttons/Button";
+import { usePaymentContext } from "@/app/context/paymentContext";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+//Todo: Add this fields to the tour DTO
+const tourAverageRating = 4.6;
+const tourType = "Walking tour";
 
-const tour = {
-  title: "Sofia Theaters",
-  description: "Sofia Theaters",
-  location: "Sofia, Bulgaria",
-  duration: 90,
-  price: 9.99,
-  type: "Walking tour",
-};
 function TourCard() {
+  const { getTourData, tour } = usePaymentContext();
+  const searchParams = useSearchParams();
+
+  const tourId = searchParams.get("tourId");
+  if (tourId === undefined || tourId === null || tourId === "") {
+    throw new Error(
+      "Payment page must have a tourId passed as a query parameter!"
+    );
+  }
+  useEffect(
+    () => async () => {
+      const data = await getTourData(tourId);
+    },
+    []
+  );
   return (
     <section
       className=" hidden web:flex flex-col  shrink-0 self-start
@@ -21,27 +34,27 @@ function TourCard() {
     >
       <Image
         className=" rounded-b-[5px] rounded-t-[15px] max-h-[161px] object-center object-cover "
-        src="https://selfguidedstorage.blob.core.windows.net/landmark-resources/ec1c648c-d033-41f4-a73e-abe0046a15f2.jpg"
-        alt={tour.title}
+        src={tour?.thumbnailImageUrl}
+        alt={tour?.title}
         width={582}
         height={161}
       />
       <div className=" m-5 mb-7 mt-4">
         <div className=" flex flex-row justify-between text-[24px] m-2 font-medium  ">
-          <h1>{tour.title}</h1>
-          <StarRating rating={4.6} />
+          <h1>{tour?.title}</h1>
+          <StarRating rating={tourAverageRating} />
         </div>
 
         <IconsBar
-          tourType={tour.type}
-          duration={tour.duration}
-          location={tour.location}
+          tourType={tourType}
+          duration={tour?.estimatedDuration}
+          location={tour?.destination}
           styles="mt-7"
         />
 
         <div className="flex justify-between mt-6 text-[24px] mb-16 font-medium  ">
           <p>Total</p>
-          <h2>USD {tour.price}</h2>
+          <h2>USD {tour?.price}</h2>
         </div>
         <div className=" text-center">
           <Button variant="primary-very-long" text="Confirm and pay" />
