1ac37711d2797d9968eec1138a45d0ed3a2339ca
Михаел Йорданов <gaws@mail.bg>
2024-08-03 08:16:56 +0300

    Create CloseButton component for payment page

--- app/app/components/Buttons/CloseButton.jsx
+++ app/app/components/Buttons/CloseButton.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import X from "../../public/svg/X.svg";
-function XButton({ onClick }) {
+function XButton({ onClick, text = "Cancel" }) {
   return (
     <button type="button" className="flex gap-2" onClick={onClick}>
-      <Image src={X} alt="Close" width={24} height={24} />
-      <p className="hidden tablet:flex">Cancel</p>
+      <Image src={X} alt={text} width={24} height={24} />
+      <p className="hidden tablet:flex">{text}</p>
     </button>
   );
 }
