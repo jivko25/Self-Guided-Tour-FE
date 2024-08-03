9d7cfc08a6cb3107ffd9bf21c8e759fb4af4c6bd
Михаел Йорданов <gaws@mail.bg>
2024-08-03 22:32:29 +0300

    Add alt prop to Images

--- app/app/components/PaymentPage/PaymentPageComponents/Card/IconsBar.jsx
+++ app/app/components/PaymentPage/PaymentPageComponents/Card/IconsBar.jsx
@@ -8,15 +8,15 @@ function IconsBar({ tourType, duration, location, styles }) {
       className={`flex justify-between border-b pb-6 border-b-[#D1D0D8] ${styles} gap-4 text-nowrap h-[64px] w-[534px]`}
     >
       <li className="flex justify-center items-center gap-1">
-        <Image src={Walk} />
+        <Image src={Walk} alt="Walk" />
         <p>{tourType}</p>
       </li>
       <li className="flex justify-center items-center gap-1">
-        <Image src={Time} />
+        <Image src={Time} alt="Time" />
         <p>Average of {duration} min</p>
       </li>
       <li className="flex justify-center items-center gap-1">
-        <Image src={Location} />
+        <Image src={Location} alt="Location" />
         <p>{location}</p>
       </li>
     </ul>
