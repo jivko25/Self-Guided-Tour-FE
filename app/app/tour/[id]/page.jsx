129bd1364c075ed6536bf9c563c236d2e9c81223
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-06 13:43:33 +0300

    Ready-made styles for the web

--- app/app/tour/[id]/page.jsx
+++ app/app/tour/[id]/page.jsx
@@ -1,5 +1,9 @@
 "use client";
+import ArrowRedoOutline from "@/app/components/Svg/ArrowRedoOutline";
+import ArrowUndoOutline from "@/app/components/Svg/ArrowUndoOutline";
+import LocationSharp from "@/app/components/Svg/LocationSharp";
 import Star from "@/app/components/Svg/Star";
+import Walk from "@/app/components/Svg/walk";
 import { useParams, useSearchParams } from "next/navigation";
 import React from "react";
 
@@ -8,7 +12,9 @@ function TourDetails() {
   const searchParams = useSearchParams();
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="flex flex-col w-[65%] justify-center items-center">
+      <div className="flex flex-col  justify-center items-center
+      web:w-[80%]
+      ">
         <div className="flex justify-start items-center w-full gap-[30px]">
           <h1 className="text-[#081120] text-[39px] font-medium font-['Inter'] leading-[58.50px]">
             Sofia Theaters
@@ -50,9 +56,11 @@ function TourDetails() {
         </div>
       </div>
 
-      <div className="flex items-center justify-around w-[65%] h-[250px] border-b-2  border-[#d1d0d8]">
-        <div className="">
-          <img src="" alt="" />
+      <div className="flex items-center justify-around  border-b-2  border-[#d1d0d8]
+      web:w-[80%] web:h-[250px]
+      ">
+        <div className="flex">
+          <Walk />
           <div>
             <h2 className="text-[#081120] text-xl font-medium font-['Inter']">
               This is a 2.5km walking tour
@@ -62,8 +70,8 @@ function TourDetails() {
             </p>
           </div>
         </div>
-        <div>
-          <img src="" alt="" />
+        <div className="flex">
+          <LocationSharp />
           <div>
             <h2 className="text-[#081120] text-xl font-medium font-['Inter']">
               This tour is located in Sofia
@@ -75,21 +83,30 @@ function TourDetails() {
         </div>
       </div>
 
-      <div className="flex items-center justify-evenly w-full h-[500px]">
-        <div className="flex flex-col gap-[50px] w-[733px]">
+      <div className="flex justify-between flex-start border-b-2 border-[#d1d0d8] 
+      web:w-[80%] web: mt-[100px]
+      ">
+        <div className="flex flex-col  h-full 
+        web:gap-[50px] web:w-[733px]
+        ">
           <div className="">
-            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9">
+            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9  
+            web:mb-[20px]
+            ">
               About the tour
             </h3>
-            <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
+            <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal ">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
               atque tenetur quos in esse ipsam eos officiis, asperiores facilis
               ad alias sint quod mollitia doloribus ipsa facere quas sunt
-              consequatur.
+              consequatur.Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Quidem atque tenetur quos in esse ipsam eos officiis,
+              asperiores facilis ad alias sint quod mollitia doloribus ipsa
+              facere quas sunt consequatur.
             </p>
           </div>
-          <div className="">
-            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9">
+          <div className="web:w-[430px] web:h-[315px]">
+            <h3 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9 web:mb-[20px]">
               Directions to starting point
             </h3>
             <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
@@ -101,9 +118,9 @@ function TourDetails() {
           </div>
         </div>
 
-        <div className="w-[430px]">
+        <div className="flex flex-col  h-full items-start justify-center web:gap-[30px] web:w-[430px]">
           <div>
-            <h2 className="text-[#081120] text-[31px] font-medium font-['Inter']">
+            <h2 className="text-[#081120] text-[31px] font-medium font-['Inter']  mb-[20px]">
               Sofia Theaters Tour
             </h2>
             <div className="flex">
@@ -116,33 +133,50 @@ function TourDetails() {
           </div>
 
           <div>
-            <h2 className="text-[#081120] text-2xl font-medium font-['Inter']">
+            <h2 className="text-[#081120] text-2xl font-medium font-['Inter'] web:mb-[10px]">
               USD 9.99
             </h2>
-            <p className="w-[430px] text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
+            <p className=" text-[#13294b] text-base font-normal font-['Inter'] leading-normal web:mb-[30px] web:w-[430px]">
               Explore the beautiful buildings and green parks of Sofia’s biggest
               theaters. Take a walk trough a beautiful sightseeing in the
               historical center of the city.
             </p>
-            <button className="rounded-[5px] px-4 py-3 bg-[#13294b] w-[430px] h-11 text-center text-white text-base font-semibold font-['Inter']">
+            <button className="rounded-[5px] px-4 py-3 bg-[#13294b]  text-center text-white text-base font-semibold font-['Inter'] 
+            web:w-[430px] web:h-11
+            ">
               Buy Tour
             </button>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col items-center gap-[50px] h-[500px]">
+      <div className="flex flex-col items-center relative
+      web:mt-[100px] web:gap-[50px] web:h-[500px]
+      ">
         <h1 className="text-[#081120] text-2xl font-medium font-['Inter'] leading-9">
           How Jauntster works
         </h1>
-        <div className="flex gap-[30px]">
-          <div className="flex items-center justify-evenly w-[733px] h-[278px] bg-neutral-50 rounded-[5px] border border-[#d1d0d8]">
-            <img src="" alt="" />
-            <div className="mt-[30px]">
-              <h1 className="mb-[20px] text-[#081120] text-xl font-medium font-['Inter'] leading-[30px]">
+        <div className="flex 
+        web:gap-[30px]
+        ">
+          <div className="flex items-center justify-evenly  bg-neutral-50 rounded-[5px] border border-[#d1d0d8] 
+          web:w-[733px] web:h-[278px]
+          ">
+            <div className="absolute flex bg-[#E7EAED] rounded-full 
+            web:top-[40px] web:px-[15px] web:py-[30px]
+            ">
+              <ArrowUndoOutline />
+              <ArrowRedoOutline />
+            </div>
+            <div className="web:mt-[30px]">
+              <h1 className=" text-[#081120] text-xl font-medium font-['Inter'] leading-[30px]
+              web:mb-[20px]
+              ">
                 How to get the most of the tour and its story
               </h1>
-              <p className="w-[667px] h-[95px] text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
+              <p className="text-[#13294b] text-base font-normal font-['Inter'] leading-normal
+              web:w-[667px] web:h-[95px]
+              ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
                 hic laudantium ex ut labore eius quas quidem, optio eos porro
                 dolores officiis iure molestias! Tempore ut optio placeat fugit
@@ -150,13 +184,25 @@ function TourDetails() {
               </p>
             </div>
           </div>
-          <div className="flex items-center justify-evenly w-[733px] h-[278px] bg-neutral-50 rounded-[5px] border border-[#d1d0d8]">
-            <img src="" alt="" />
-            <div className="mt-[30px]">
-              <h1 className="mb-[20px] text-[#081120] text-xl font-medium font-['Inter'] leading-[30px]">
+
+          <div className="flex items-center justify-evenly bg-neutral-50 rounded-[5px] border border-[#d1d0d8]  
+          web:w-[733px] web:h-[278px]
+          ">
+            <div className="absolute flex bg-[#E7EAED] rounded-full 
+            web:top-[40px] web:px-[15px] web:py-[30px]
+            ">
+              <ArrowUndoOutline />
+              <ArrowRedoOutline />
+            </div>
+            <div className="web:mt-[30px]">
+              <h1 className=" text-[#081120] text-xl font-medium font-['Inter'] leading-[30px]
+              web:mb-[20px]
+              ">
                 How to get the most of the tour and its story
               </h1>
-              <p className="w-[667px] h-[95px] text-[#13294b] text-base font-normal font-['Inter'] leading-normal">
+              <p className="text-[#13294b] text-base font-normal font-['Inter'] leading-normal
+              web:w-[667px] web:h-[95px]
+              ">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
                 hic laudantium ex ut labore eius quas quidem, optio eos porro
                 dolores officiis iure molestias! Tempore ut optio placeat fugit
