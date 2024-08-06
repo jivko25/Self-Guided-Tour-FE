6eb24703740e2e7e86d045545bebd39fedd5bded
ajummer <makeudreamempty@gmail.com>
2024-08-06 11:36:45 +0300

    feat: add admin panel with dummy data

--- app/app/admin/tours/page.jsx
+++ app/app/admin/tours/page.jsx
@@ -7,6 +7,9 @@ import AdminPanelTabs from "@/app/components/Admin/AdminPanelTabs/AdminPanelTabs
 
 import CheckmarkIcon from "../../public/svg/checkmark.svg";
 import CloseIcon from "../../public/svg/close-red.svg";
+import MobilePagination from "@/app/components/Admin/Pagination/MobilePagination.jsx";
+import WebPagination from "@/app/components/Admin/Pagination/WebPagination.jsx";
+import TourStatus from "@/app/components/TourStatus/TourStatus.jsx";
 
 const PendingTours = () => {
   const [tours, setTours] = useState([]);
@@ -32,27 +35,52 @@ const PendingTours = () => {
     console.log("Declined tour with ID:", tourId);
   };
 
+  const tableHeaders = [
+    { label: "Tour Title", additionalClasses: "" },
+    { label: "Date", additionalClasses: "smallPhone:hidden tablet:table-cell" },
+    {
+      label: "Creator",
+      additionalClasses: "smallPhone:hidden web:table-cell",
+    },
+    {
+      label: "Status",
+      additionalClasses: "smallPhone:hidden tablet:table-cell",
+    },
+    { label: "Action", additionalClasses: "rounded-tr-[5px]" },
+  ];
+
   return (
-    <div className="container max-w-[1349px] my-16 mx-auto p-4 font-['Inter']">
+    <div className="container max-w-[1349px] mt-20 mx-auto p-4  text-base font-normal  leading-none font-['Inter']">
       <AdminPanelTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       <table className="min-w-full bg-white border">
         <thead>
           <tr className="h-12 bg-[#b6bdc7]">
-            <th className="py-2 px-4 border-b text-[#081120] text-[13px] font-semibold  leading-none ">
-              Tour Name
-            </th>
-            <th className="py-2 px-4 border-b text-[#081120] text-[13px] font-semibold  leading-none rounded-tr-[5px]">
-              Actions
-            </th>
+            {tableHeaders.map((header, index) => (
+              <th
+                key={index}
+                className={`py-2 px-4 ${header.additionalClasses}`}
+              >
+                {header.label}
+              </th>
+            ))}
           </tr>
         </thead>
-        <tbody>
+        <tbody className="p-4">
           {tours.map((tour) => (
-            <tr key={tour.id}>
-              <td className="py-2 px-4 border-b text-center ">{tour.name}</td>
-              <td className="py-2 px-10 border-b text-right">
+            <tr key={tour.id} className="border-b-2 ">
+              <td className=" text-center  ">{tour.name}</td>
+              <td className="text-center smallPhone:hidden tablet:table-cell ">
+                01.01.2024
+              </td>
+              <td className="text-center smallPhone:hidden web:table-cell ">
+                Ivan Ivanov
+              </td>
+              <td className="text-center smallPhone:hidden tablet:table-cell ">
+                <TourStatus status="Under Review" />
+              </td>
+              <td className="py-2   text-center  ">
                 <button
-                  className="w-[38px] h-[30px] rounded-[5px] border-2 border-[#027e00] mr-6"
+                  className="w-[38px] h-[30px] rounded-[5px] border-2 border-[#027e00] mr-6 tablet:h-10 tablet:w-[48px]"
                   onClick={() => handleAccept(tour.id)}
                 >
                   <Image
@@ -63,7 +91,7 @@ const PendingTours = () => {
                   />
                 </button>
                 <button
-                  className="w-[38px] h-[30px] rounded-[5px] border-2 border-[#e80000]"
+                  className="w-[38px] h-[30px] rounded-[5px] border-2 border-[#e80000] tablet:h-10 tablet:w-[48px]"
                   onClick={() => handleDecline(tour.id)}
                 >
                   <Image
@@ -78,6 +106,8 @@ const PendingTours = () => {
           ))}
         </tbody>
       </table>
+      <MobilePagination />
+      <WebPagination />
     </div>
   );
 };
---
+++ app/app/components/Admin/Pagination/WebPagination.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image.js";
+
+import PrevPageIcon from "../../../public/svg/chevron-left.svg";
+import NextPageIcon from "../../../public/svg/chevron-right.svg";
+import DownIcon from "../../../public/svg/chevron-down.svg";
+
+export default function WebPagination() {
+  return (
+    <div className="w-full h-[60px] flex justify-between items-center px-10 py-4 bg-[#b6bdc7] text-[#081120]  text-base font-normal rounded-tl-[5px] rounded-tr-[5px] smallPhone:hidden tablet:flex ">
+      <section className="ml-10">
+        <p>3 of 200 results</p>
+      </section>
+      <section className="flex gap-4 align-baseline  ">
+        <p className="my-auto">You are on page</p>
+        <div className="w-16 h-9 flex justify-between p-2 items-center bg-neutral-50 rounded-[5px]">
+          <span className="text-[25px] ">1</span>
+          <div className="w-5 h-5 relative">
+            <Image src={DownIcon} width={40} height={36} />
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <button>
+            {" "}
+            <Image src={PrevPageIcon} width={40} height={36} />
+          </button>
+          <button>
+            {" "}
+            <Image src={NextPageIcon} width={40} height={36} />
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
