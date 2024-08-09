76bd1be5d5c01e2436c514734f1c73964f96bd1c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-09 16:03:17 +0300

    Update LocationSharp.jsx

--- app/app/components/Svg/LocationSharp.jsx
+++ app/app/components/Svg/LocationSharp.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function LocationSharp() {
   return (
     <svg
-      width="40"
-      height="40"
+      width="50"
+      height="50"
       viewBox="0 0 60 60"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
