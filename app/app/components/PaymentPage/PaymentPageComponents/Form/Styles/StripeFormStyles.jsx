5d6da33e39be7c2be9bb29ee032c3c017a0d8a11
Михаел Йорданов <gaws@mail.bg>
2024-08-13 15:49:36 +0300

    add styles to the stripe form

---
+++ app/app/components/PaymentPage/PaymentPageComponents/Form/Styles/StripeFormStyles.jsx
@@ -0,0 +1,12 @@
+export const appearance = {
+  theme: "stripe",
+  variables: {
+    fontFamily: "Inter, sans-serif",
+    fontLineHeight: ".5",
+    borderRadius: "1px",
+    accessibleColorOnColorPrimary: "#262626",
+    gridRowSpacing: "4rem",
+    gridColumnSpacing: "rem",
+    spacingUnit: "0.4rem",
+  },
+};
