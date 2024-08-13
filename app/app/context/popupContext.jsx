3be54e637518fcde1d06e63799b175d4ccdc7797
Anton <140314098+AntonMurtin@users.noreply.github.com>
2024-08-13 13:33:56 +0300

    add popup

---
+++ app/app/context/popupContext.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { createContext, useContext, useReducer } from 'react';
+import { v4 } from 'uuid';
+import { AnimatePresence } from 'framer-motion';
+import Popup from '../components/Notification/Popup';
+
+
+const PopupContext = createContext();
+
+export const PopupProvider = ({ children }) => {
+
+    const popupReduser = (state, action) => {
+        switch (action.type) {
+            case 'ADD':
+                return [...state, { ...action.payload }];
+
+            case 'REMOVE':
+                return state.filter(x => x.id !== action.id);
+
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(popupReduser, []);
+
+    return (
+        <PopupContext.Provider value={dispatch}>
+            <div className="z-30 fixed top-[20px] ">
+            <AnimatePresence>
+                    {state.map(x => (
+                        <Popup dispatch={dispatch} key={x.id} {...x} />
+                    ))}
+                </AnimatePresence>
+            </div>
+            {children}
+
+        </PopupContext.Provider>
+    )
+}
+
+export const usePopup = () => {
+    const dispatch = useContext(PopupContext);
+
+    return (props) => {
+        dispatch({
+            type: 'ADD',
+            payload: {
+                id: v4(),
+                ...props
+            }
+        })
+    }
+}
