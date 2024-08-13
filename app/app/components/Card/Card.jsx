6b89be1bdcab651e2b7b950e8031bbbe2e6c52ac
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-08-13 21:07:44 +0300

    applied some fixes

--- app/app/components/Card/Card.jsx
+++ app/app/components/Card/Card.jsx
@@ -9,6 +9,7 @@ export default function Card({
   description,
   price,
   rating,
+  classes
 }) {
   return (
     <>
@@ -23,16 +24,16 @@ export default function Card({
           </div>
         </div>
       ) : (
-        <div className=" w-[176px] h-[224px] tablet:w-[282px] tablet:h-[492px] web:w-[430px] web:h-[522px] bg-[#FAFAFA] border border-[#D1D0D8] rounded-[5px] tablet:rounded-[15px]">
-          <div className="mb-[6px] tablet:mb-[8.5px] w-full h-[116px] tablet:h-[262px] web:h-[256px]">
+        <div className={`w-[176px] h-[224px] tablet:w-[282px] tablet:h-[492px] web:w-[430px] web:h-[522px] bg-[#FAFAFA] border border-[#D1D0D8] rounded-[5px] tablet:rounded-[15px] ${classes}`}>
+          <div className="mb-[6px] tablet:mb-[8.5px] w-full h-[110px] tablet:h-[262px] web:h-[256px]">
             <Image
               src={imageSrc}
               alt="Card image"
-              className="rounded-t-[5px] tablet:rounded-t-[15px] rounded-b-[5px] w-full h-[116px] tablet:h-[262px] web:h-[256px]"
+              className="rounded-t-[5px] tablet:rounded-t-[15px] rounded-b-[5px] w-full h-[110px] tablet:h-[262px] web:h-[256px]"
               style={{objectFit:'cover'}}
             />
           </div>
-          <div className="flex flex-col pl-[8px] tablet:pl-[24px] pb-[12px] web:pb-[24px] pt-[6px] tablet:pr-[12px] web:pr-[8px] text-[#13294B]">
+          <div className="flex flex-col pl-[8px] tablet:pl-[24px] pb-[12px] web:pb-[24px] tablet:pr-[12px] web:pr-[8px] text-[#13294B]">
             <div className="flex justify-between text-[13px] tablet:text-[16px] mb-[6px] web:mb-[18px] pr-[6px] tablet:pr-[16px]">
               <span className="w-[151px]">{location}</span>
               <div className="w-[143px] flex justify-end">
@@ -44,7 +45,7 @@ export default function Card({
                 />
               </div>
             </div>
-            <span className="text-[#081120] text-[16px] tablet:text-[25px] tablet:font-semibold">
+            <span className="mb-[12px] tablet:mb-[6px] text-[#081120] text-[16px] tablet:text-[25px] tablet:font-semibold">
               {name}
             </span>
             <div className="hidden tablet:block tablet:h-[90px] web:h-[108px]">
