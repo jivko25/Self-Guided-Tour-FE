e9ee2452f513c9090726f946181178f32d6f0c08
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-08-20 23:06:41 +0300

    feature: created explore page layout

--- app/app/explore/page.jsx
+++ app/app/explore/page.jsx
@@ -1,23 +1,45 @@
-"use client"
+"use client";
 import { axiosTour } from "@/api/axios";
 import { useEffect, useState } from "react";
 import Card from "../components/Card/Card";
-
+import Search from "../components/Search/Search";
 
 export default function Explore() {
-    const [tours, setTours] = useState([]);
-    const [pageNumber, setPageNumber] = useState(1);
-    const [pageSize, setPageSize] = useState(8);
-    const [searchTerm, setSearchTerm] = useState();
+  const [tours, setTours] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(8);
+  const [searchTerm, setSearchTerm] = useState();
 
-    useEffect(() => {
-        axiosTour.get('/')
-                    .then(data => setTours([...data.data.result]))
-                    .catch(err => console.log(err));
-    }, []);
-    return(
-        <>
-        {(tours.length > 0) && tours.map(t => <Card key={t.tourId} title={t.title} imageSrc={t.thumbnailImageUrl}/>)}
-        </>
-    );
+  useEffect(() => {
+    axiosTour
+      .get(`?pageSize=${pageSize}&pageNumber=${pageNumber}`)
+      .then((data) => setTours([...data.data.result]))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="flex flex-col items-center mt-20 mb-20 tablet:mt-40 tablet:mb-0">
+      <h1 className="mb-6 tablet:mb-16 text-xl tablet:text-3xl web:text-[39px] font-medium">
+        Discover your next exciting trip
+      </h1>
+      <div className="mb-4 tablet:mb-[108px] web:mb-[136px]">
+        <Search />
+      </div>
+      <h2 className="hidden web:inline-block self-start mb-9 text-[31px] font-medium">Explore Top Rated trips</h2>
+      <div className="grid grid-cols-2 web:grid-cols-3 webl:grid-cols-4 gap-x-[9px] tablet:gap-x-5 web:gap-x-6 gap-y-6 tablet:gap-y-16">
+        {tours.length > 0 &&
+          tours.map((tour) => (
+            <Card
+              classes={"tablet:w-[383px]"}
+              key={tour.tourId}
+              title={tour.title}
+              imageSrc={tour.thumbnailImageUrl}
+              description={tour.summary}
+              location={tour.destination}
+              price={`EUR ${tour.price}`}
+              rating={4.5}
+            />
+          ))}
+      </div>
+    </div>
+  );
 }
