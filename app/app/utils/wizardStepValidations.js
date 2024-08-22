b14bed783b202e6fc091fdf79eb7ac4190719f6d
ajummer <makeudreamempty@gmail.com>
2024-08-22 20:06:01 +0300

    chore: remove  location description validation

--- app/app/utils/wizardStepValidations.js
+++ app/app/utils/wizardStepValidations.js
@@ -1,3 +1,47 @@
+export const isArrayOfObjFilled = (input) => {
+  console.log("Input to isObjectFilled:", input);
+
+  // Function to check if a single object is fully filled
+  const checkObjectFilled = (obj) => {
+    // Check for required fields and non-empty values
+    if (
+      !obj.hasOwnProperty("locationCity") || // Check if locationCity key is missing
+      !obj.locationCity || // Check if locationCity is empty or undefined
+      !obj.hasOwnProperty("addFields") || // Check if addFields key is missing
+      !Array.isArray(obj.addFields) || // Check if addFields is not an array
+      obj.addFields.length === 0 // Check if addFields is an empty array
+    ) {
+      return false;
+    }
+
+    // Check that no field is null, undefined, or an empty string
+    for (let key in obj) {
+      if (
+        obj[key] === null ||
+        obj[key] === undefined ||
+        obj[key] === "" ||
+        (Array.isArray(obj[key]) && obj[key].length === 0)
+      ) {
+        // Skip the locationDescription field from being checked
+        if (key === "locationDescription") {
+          continue;
+        }
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  // If the input is an array, check each object in the array
+  console.log(Array.isArray(input));
+  if (Array.isArray(input)) {
+    return input.length > 0 && input.every(checkObjectFilled);
+  }
+  // If the input is not an array, handle it as a single object
+  return checkObjectFilled(input);
+};
+
 export const validateStep = (step, formData, targetStep) => {
   const isObjectFilled = (obj) => {
     for (let key in obj) {
@@ -31,9 +75,9 @@ export const validateStep = (step, formData, targetStep) => {
               placeId: loc.placeId,
               latitude: loc.latitude,
               longitude: loc.longitude,
-              // locationCity: loc.locationCity,
+              locationCity: loc.locationCity,
+              addFields: loc.addFields,
               // locationDescription: loc.locationDescription,
-              // addFields: loc.addFields,
             })
           )
         );
@@ -47,6 +91,7 @@ export const validateStep = (step, formData, targetStep) => {
         formData.step3Data.locationDescription &&
         formData.step3Data.addFields.length > 0
       );
+
     default:
       return true;
   }
