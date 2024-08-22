15fd3a3b87a4c8394c9b972951e53fe63cbf4041
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 09:04:11 +0300

    Update MailOutline.jsx

--- app/app/components/Svg/MailOutline.jsx
+++ app/app/components/Svg/MailOutline.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function MailOutline() {
+function MailOutline({ className }) {
   return (
     <svg
+      className={`${className}`}
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -13,16 +14,18 @@ function MailOutline() {
         <path
           id="Vector"
           d="M19.875 4.5H4.125C3.08947 4.5 2.25 5.33947 2.25 6.375V17.625C2.25 18.6605 3.08947 19.5 4.125 19.5H19.875C20.9105 19.5 21.75 18.6605 21.75 17.625V6.375C21.75 5.33947 20.9105 4.5 19.875 4.5Z"
-          stroke="white"
           strokeLinecap="round"
           strokeLinejoin="round"
+          className="stroke-current"
+          strokeMiterlimit="10"
         />
         <path
           id="Vector_2"
           d="M5.25 7.5L12 12.75L18.75 7.5"
-          stroke="white"
           strokeLinecap="round"
           strokeLinejoin="round"
+          className="stroke-current"
+          strokeMiterlimit="10"
         />
       </g>
     </svg>
