e9fb1b6a440f560e990a404b585bda11ff7f27dd
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-08-24 10:46:16 +0300

    feature: improved error handling in explore

--- app/app/explore/page.jsx
+++ app/app/explore/page.jsx
@@ -5,20 +5,34 @@ import Card from "../components/Card/Card";
 import Search from "../components/Search/Search";
 import { useRouter, useSearchParams } from "next/navigation";
 import ButtonRound from "../components/Buttons/ButonRound";
+import { usePopup } from "@/app/context/popupContext.jsx";
 
 export default function Explore() {
   const page = useSearchParams().get('page') || 1;
   const [tours, setTours] = useState([]);
-  const [pageSize, setPageSize] = useState(8);
+  const [pageSize, setPageSize] = useState(12);
   const [searchTerm, setSearchTerm] = useState();
   const router = useRouter();
+  const popup = usePopup();
 
   useEffect(() => {
 
     axiosTour
       .get(`?pageSize=${pageSize}&pageNumber=${page}`)
-      .then((data) => setTours([...data.data.result]))
-      .catch((err) => console.log(err));
+      .then((data) => {
+          const dataResult = data.data.result;
+          setTours([...data.data.result.tours]);
+      })
+      .catch((err) => {
+        const errors = err.response.data.errors;
+        for (const key in errors) {
+          popup({
+            type: "ERROR",
+            message: errors[key],
+          });
+        }
+        
+      });
   }, [page]);
 
   const handlePrevPage = () => {
@@ -42,11 +56,11 @@ export default function Explore() {
       <h2 className="hidden web:inline-block self-start mb-9 text-[31px] font-medium">
         Explore Top Rated trips
       </h2>
-      <div className="grid grid-cols-1 phone:grid-cols-2 web:grid-cols-3 webl:grid-cols-4 gap-x-[9px] tablet:gap-x-5 web:gap-x-6 gap-y-6 tablet:gap-y-16 mb-16 web:mb-[108px]">
+      <div className="grid grid-cols-1 phone:grid-cols-2 web:grid-cols-4 webl:grid-cols-4 gap-x-[9px] tablet:gap-x-5 web:gap-x-6 gap-y-6 tablet:gap-y-16 mb-16 web:mb-[108px]">
         {tours.length > 0 &&
           tours.map((tour) => (
             <Card
-              classes={"tablet:w-[383px] cursor-pointer"}
+              classes={"cursor-pointer"}
               key={tour.tourId}
               title={tour.title}
               imageSrc={tour.thumbnailImageUrl}
