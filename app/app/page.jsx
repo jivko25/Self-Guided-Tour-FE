61815439651d3920a072ca2ad49e0cae54f05ed5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-27 16:23:33 +0300

    Update page.jsx

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -2,7 +2,7 @@ import HomePage from "./components/HomePage/HomePage";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center flex-col min-h-screen">
+    <main className="flex items-center justify-center flex-col w-full min-h-screen">
       <HomePage/>
     </main>
   );
