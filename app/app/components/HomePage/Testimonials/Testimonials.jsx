c4a9f0fcf86f03219c8123d2a74849b9dd201042
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-29 11:38:07 +0300

    Update Testimonials.jsx

--- app/app/components/HomePage/Testimonials/Testimonials.jsx
+++ app/app/components/HomePage/Testimonials/Testimonials.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 
 function Testimonials() {
+  const [bulgarianBestPlaces, setBulgarianBestPlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchTours = async () => {
+      try {
+        const resBulgarian = await axiosTour.get(
+          "?sortBy=mostBought&pageNumber=1&pageSize=4"
+        );
+        setBulgarianBestPlaces(resBulgarian.data.result.tours);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchTours();
+  }, []);
   return (
     <div>Testimonials</div>
   )
