0e51e75c39b9392c8f939755b184753fa20b94e5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-29 13:58:18 +0300

    Update HowItWorks.jsx

--- app/app/components/HomePage/HowItWorks/HowItWorks.jsx
+++ app/app/components/HomePage/HowItWorks/HowItWorks.jsx
@@ -1,57 +1,40 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import imgFirst from "../../../public/images/HomePageImg1.png";
-import imgSecond from "../../../public/images/HomePageImg2.jpeg";
+import imgGroup from "../../../public/images/Group35.png";
+
 import Image from "next/image";
 import Btn from "../../Buttons/Btn";
 function HowItWorks() {
   return (
     <div
-      className="w-full flex items-center justify-center bg-[#13294b] h-full
-    web:min-h-[922px] web:flex-row web:mt-[0px] web:justify-center
-    tablet:min-h-[573px] tablet:flex-row tablet:mt-[0px] tablet:justify-center
-    phone:min-h-[649px] phone:flex-col phone:mt-[30px] phone:justify-evenly
-    smallPhone:min-h-[649px] smallPhone:flex-col smallPhone:mt-[30px] smallPhone:justify-center
+      className="w-full flex items-center bg-[#13294b] p-[5%]
+    web:flex-row web:mt-[0px] web:justify-center web:gap-[50px] web:h-[992px]
+    tablet:flex-row tablet:mt-[0px] tablet:justify-center tablet:gap-[50px] tablet:h-[573px]
+    phone:flex-col phone:mt-[30px] phone:justify-center phone:gap-[20px] phone:min-h-[395px]
+    smallPhone:flex-col smallPhone:mt-[30px] smallPhone:justify-center smallPhone:gap-[10px] smallPhone:min-h-[638px]
     "
     >
-      <div className="relative flex items-center justify-center w-full h-full 
-            web:min-h-[922px]
-            tablet:min-h-[573px]
-            phone:min-h-[324px]
-            smallPhone:min-h-[324px]
-      ">
+      <div className="flex items-center justify-center w-full h-full">
         <Image
-          className="absolute rounded-[15px] z-10 object-cover w-full
-          webl:max-w-[408px] webl:h-[440px] webl:top-[60px]   webl:left-[60px]
-          web:max-w-[408px] web:h-[440px] web:top-[60px]   web:left-[60px]
-          tablet:max-w-[156px] tablet:h-[169px] tablet:top-[60px]   tablet:left-[60px]
-          phone:max-w-[156px] phone:h-[169px] phone:top-[30px]  phone:left-[60px]
-          smallPhone:max-w-[114px] smallPhone:h-[123px] smallPhone:top-[30px]  smallPhone:left-[60px]
-          
+          className="object-cover 
+          webl:w-[75%] 
+          web:w-[85%] 
+          tablet:w-[450px]
+          phone:w-[350px]
+          smallPhone:w-[280px]
           "
-          src={imgFirst}
+          src={imgGroup}
           alt="Description of image 1"
         />
-        <Image
-          className="absolute  rounded-[15px]  object-cover w-full
-          webl:max-w-[581px] webl:h-[700px] webl:top-[120px]  webl:right-[60px]
-          web:max-w-[581px] web:h-[700px] web:top-[120px]  web:right-[60px]
-          tablet:max-w-[223px] tablet:h-[269px] tablet:top-[120px]  tablet:right-[60px]
-          phone:max-w-[223px] phone:h-[269px] phone:top-[60px]  phone:right-[60px]
-          smallPhone:max-w-[162px] smallPhone:h-[195px] smallPhone:  smallPhone:right-[60px]
-         
-          "
-          src={imgSecond}
-          alt="Description of image 2"
-        />
+        
       </div>
       <div
-        className="flex justify-center w-full items-start flex-col h-full
-          web:gap-[30px] web:items-start web:min-h-[922px]
-          tablet:gap-[7px] tablet:items-start tablet:min-h-[573px]
-          phone:gap-[10px] phone:items-center phone:min-h-[324px]
-          smallPhone:gap-[3px] smallPhone:items-center smallPhone:min-h-[324px]
+        className="flex justify-center items-start flex-col h-full w-full 
+          web:gap-[30px] web:items-start web:min-h-[922px] 
+          tablet:gap-[7px] tablet:items-start tablet:min-h-[573px] 
+          phone:gap-[10px] phone:items-center phone:min-h-[324px] 
+          smallPhone:gap-[3px] smallPhone:items-center smallPhone:min-h-[324px] 
       
       "
       >
