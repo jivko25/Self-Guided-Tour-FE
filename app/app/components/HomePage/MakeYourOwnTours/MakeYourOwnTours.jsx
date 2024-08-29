5bfe9816e6e7dd3cdddf2e6c62c86c530bac46ed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-29 17:01:45 +0300

    Update MakeYourOwnTours.jsx

--- app/app/components/HomePage/MakeYourOwnTours/MakeYourOwnTours.jsx
+++ app/app/components/HomePage/MakeYourOwnTours/MakeYourOwnTours.jsx
@@ -1,48 +1,51 @@
 import React from "react";
-import VECTOR1 from "../../../public/Home/Vector 1.svg";
-import VECTOR7 from "../../../public/Home/Vector 7.svg";
-import VECTOR3 from "../../../public/Home/Vector 3.svg";
-import VECTOR8 from "../../../public/Home/Vector 8.svg";
-import VECTOR5 from "../../../public/Home/Vector 5.svg";
-import CREATE from "../../../public/Home/Group 42.svg";
-import PLAN from "../../../public/Home/Group 36.svg";
-import DESCRIBE from "../../../public/Home/Group 39.svg";
-import REVIEW from "../../../public/Home/Group 44.svg";
+import makeYourOwnToursImg from "../../../public/images/makeYourOwnToursImg.png";
 import Image from "next/image";
 import Link from "next/link";
 import SeeMoreSvgHomePage from "../../Svg/SeeMoreSvgHomePage";
 function MakeYourOwnTours() {
   return (
-    <div className="">
-      
-      <Link
-        className="hidden space-x-2 items-start
+    <div className="hidden  items-center justify-center w-full h-full 
+    web:flex web:min-h-[608px] web:max-w-[1792px]
+    tablet:flex tablet:min-h-[500px]
+    ">
+
+      <div className="flex flex-col items-center justify-center gap-[50px]">
+        <h1 className="text-[#081120] font-medium font-['Inter'] leading-[58.50px]
+        web:text-[39px] 
+        tablet:text-[31px]
+
+        ">Make Your Own Tours</h1>
+        <div className="w-full max-w-[1792px]">
+          <Image className="w-full" src={makeYourOwnToursImg} alt="Description of image 1" />
+        </div>
+        <Link
+          className="hidden space-x-2 justify-end w-full
           web:flex web:h-[19px]
           tablet:flex tablet:h-[19px]
-          phone:
-          smallPhone:
         "
-        href="/explore"
-      >
-        <span
-          className="text-[#081120] text-base font-semibold font-['Inter']
+          href="/explore"
+        >
+          <span
+            className="text-[#081120] text-base font-semibold font-['Inter']
             web:w-[74px] web:h-[19px] web:text-base
             tablet:w-[74px] tablet:h-[19px] tablet:text-base
             phone:
             smallPhone:
             "
-        >
-          See More
-        </span>
-        <SeeMoreSvgHomePage
-          className="hidden
+          >
+            See More
+          </span>
+          <SeeMoreSvgHomePage
+            className="hidden
             web:w-6 web:h-6 web:block
             tablet:w-6 tablet:h-6 tablet:block
             phone:
             smallPhone:
             "
-        />
-      </Link>
+          />
+        </Link>
+      </div>
     </div>
   );
 }
