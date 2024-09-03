0f1b5b8f514e138836dff7f62e3b6fabebd82ca5
ajummer <makeudreamempty@gmail.com>
2024-09-03 14:53:22 +0300

    chore: show prev button in step 3

--- app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
+++ app/app/components/CreateTourSteps/Step3/Step3Components/NavigationButtons.jsx
@@ -2,10 +2,10 @@
 import React from "react";
 import Btn from "../../../Buttons/Btn.jsx";
 
-function NavigationButtons({ prevStep, handleFinish }) {
+function NavigationButtons({ handlePrevStep, handleNextStep }) {
   return (
     <div
-      className="flex items-start w-full h-[128px] pt-[20px]
+      className="flex items-start gap-4 w-full h-[128px] pt-[20px]
             web:justify-center web:max-w-[50%] web:gap-[380px] web:bg-neutral-50 web:border-t web:border-gray-200
             tablet:justify-center tablet:gap-[350px] tablet:bg-neutral-50 tablet:border-t tablet:border-gray-200
             phone:justify-center phone:mb-[50px] phone:pt-[20px]
@@ -13,11 +13,11 @@ function NavigationButtons({ prevStep, handleFinish }) {
     >
       <Btn
         variant="transparent"
-        className="hidden text-center text-gray-900 text-base font-semibold px-4 py-3 w-[128px] h-[43px] justify-center items-center border-b-2 border-neutral-50
+        className=" text-center text-gray-900 text-base font-semibold px-4 py-3 w-[128px] h-[43px] justify-center items-center border-b-2 border-neutral-50
         web:inline-flex  
         tablet:inline-flex"
         text="Prev"
-        onClick={prevStep}
+        onClick={handlePrevStep}
       />
       <Btn
         variant="fullWidth"
@@ -27,7 +27,7 @@ function NavigationButtons({ prevStep, handleFinish }) {
         phone:px-[16px] phone:py-[12px] phone:w-[177px] 
         smallPhone:px-[16px] smallPhone:py-[12px] smallPhone:w-[130px]"
         text="Next"
-        onClick={handleFinish}
+        onClick={handleNextStep}
       />
     </div>
   );
