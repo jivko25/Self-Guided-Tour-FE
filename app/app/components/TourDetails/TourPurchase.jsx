72e62d70ac2570c41494a6284729ace1602c82ce
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-05 20:38:19 +0300

    Update TourPurchase.jsx

--- app/app/components/TourDetails/TourPurchase.jsx
+++ app/app/components/TourDetails/TourPurchase.jsx
@@ -9,6 +9,7 @@ function TourPurchase({ destination, price, id, router }) {
         web:gap-[30px] web:max-w-[430px] web:mt-[0px]
         tablet:max-w-[584px] tablet:mt-[0px]
         phone:mt-[20px] phone:w-[95%]
+        smallPhone:mt-[20px] smallPhone:w-[95%]
         "
         >
           <div
@@ -16,6 +17,7 @@ function TourPurchase({ destination, price, id, router }) {
           web:items-start web:flex-col 
           tablet:flex-row tablet:gap-[20px] tablet:justify-center
           phone:flex-col phone:mb-[20px]
+          smallPhone:flex-col smallPhone:mb-[20px]
           "
           >
             <h2
@@ -23,6 +25,7 @@ function TourPurchase({ destination, price, id, router }) {
             web:text-[31px] web:mb-[20px]
             tablet:text-2xl tablet:mb-[20px]
             phone:text-xl phone:mb-[10px]
+            smallPhone:text-xl smallPhone:mb-[10px]
             "
             >
               {destination} Theaters Tour
@@ -40,13 +43,16 @@ function TourPurchase({ destination, price, id, router }) {
             className="flex 
           web:flex-col 
           tablet:flex-col-reverse 
-          phone:flex-col-reverse phone:items-start"
+          phone:flex-col-reverse phone:items-start
+          smallPhone:flex-col-reverse smallPhone:items-start
+          "
           >
             <p
               className="flex text-[#081120] font-medium font-['Inter'] 
             web:mb-[10px] web:flex-row web:text-2xl web:gap-[10px]
             tablet:text-xl target:flex-row-reverse tablet:mb-[20px] tablet:gap-[1px]
             phone:mb-[30px] phone:flex-row-reverse phone:text-lg phone:gap-[1px]
+            smallPhone:mb-[30px] smallPhone:flex-row-reverse smallPhone:text-lg smallPhone:gap-[1px]
             "
             >
               <span>USD</span>
@@ -58,6 +64,7 @@ function TourPurchase({ destination, price, id, router }) {
             web:mb-[10px] web:max-w-[430px] web:text-base
             tablet:mb-[20px] tablet:max-w-[430px] tablet:text-base 
             phone:mb-[10px] phone:text-sm 
+            smallPhone:mb-[10px] smallPhone:text-sm 
             "
             >
               Explore the beautiful buildings and green parks of Sofia’s biggest
@@ -66,7 +73,7 @@ function TourPurchase({ destination, price, id, router }) {
             </p>
           </div>
 
-          <div className="web:w-[430px] tablet:w-[282px] phone:w-[361px]">
+          <div className="web:w-[430px] tablet:w-[282px] phone:w-[361px] smallPhone:w-[300px]">
             <Btn
               type="submit"
               variant="filled"
