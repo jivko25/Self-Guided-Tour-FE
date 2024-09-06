aa275f2872066dcacc15e1f79e5ce69a51605b58
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-06 17:19:41 +0300

    Create ImageOutline.jsx

---
+++ app/app/components/TourDetails/Svgs/ImageOutline.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function ImageOutline() {
+  return (
+    <svg
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icons / image-outline">
+        <path
+          id="Vector"
+          d="M20 3.75H5C3.75736 3.75 2.75 4.75736 2.75 6V18C2.75 19.2426 3.75736 20.25 5 20.25H20C21.2426 20.25 22.25 19.2426 22.25 18V6C22.25 4.75736 21.2426 3.75 20 3.75Z"
+          stroke="#081120"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_2"
+          d="M16.25 9.75C17.0784 9.75 17.75 9.07843 17.75 8.25C17.75 7.42157 17.0784 6.75 16.25 6.75C15.4216 6.75 14.75 7.42157 14.75 8.25C14.75 9.07843 15.4216 9.75 16.25 9.75Z"
+          stroke="#081120"
+          stroke-miterlimit="10"
+        />
+        <path
+          id="Vector_3"
+          d="M14.75 15.7405L10.5003 11.4988C10.2299 11.2284 9.86642 11.0716 9.4842 11.0602C9.10198 11.0489 8.72986 11.184 8.44391 11.4379L2.75 16.5004"
+          stroke="#081120"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          id="Vector_4"
+          d="M11 20.2501L16.7816 14.4685C17.046 14.2035 17.3999 14.047 17.7738 14.0297C18.1477 14.0123 18.5145 14.1354 18.8023 14.3747L22.25 17.2501"
+          stroke="#081120"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default ImageOutline;
