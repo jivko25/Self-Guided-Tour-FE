14ec549150fa36683a7cfddb3ec5bd04dd82ede9
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-09-18 22:25:32 +0300

    feature: completed list styles and refactored some code

--- app/app/preview/[id]/page.jsx
+++ app/app/preview/[id]/page.jsx
@@ -9,11 +9,14 @@ import { useCreateTour } from "@/app/context/createTourContext";
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { getOne } from "@/app/actions/tourActions";
+import Play from "../../public/svg/play.svg";
+import ArrowRedo from "../../public/svg/arrow-redo.svg";
 
 export default function Preview() {
   const { id } = useParams();
   const { formData } = useCreateTour();
-  const [previewData, setPreviewData] = useState([]);
+  const [title, setTitle] = useState("");
+  const [locations, setLocations] = useState([]);
   const [error, setError] = useState({});
 
   useEffect(() => {
@@ -22,9 +25,11 @@ export default function Preview() {
     } else {
       getOne(id).then((result) => {
         const { data, error } = result;
+        console.log(data);
 
         if (data) {
-          setPreviewData(data.landmarks);
+          setTitle(data.title);
+          setLocations(data.landmarks);
         } else {
           setError(error);
         }
@@ -34,16 +39,29 @@ export default function Preview() {
   return (
     <div
       className="w-[100%] h-full web:relative mt-24 tablet:mt-[151px] web:mt-[64px] 
-                px-[8px] phone:px-[16px] tablet:px-[125px] web:px-[56px] "
+                  px-[8px] phone:px-[16px] tablet:px-[125px] web:px-[56px]"
     >
+      {/* <section>
+        <h1>Tour Locations <span></span> {title}</h1>
+        <Link className="mt-[7px] tablet:mt-0 text-[#E8B600]" href={"/edit"}>
+          <Image src={Pencil} width={24} height={24} alt="pencil" />
+        </Link>
+      </section> */}
       <section
-        className="w-[100%] flex flex-col align-center text-[14px]
-                          mb-[30px] web:mb-0 font-medium text-[#081120] text-[14px] web:text-[16px]
-                          web:h-[582px] web:w-1/2"
+        className="flex flex-col align-center text-[14px]
+                    mb-[30px] web:mb-0 font-medium text-[#081120] text-[14px] web:text-[16px]
+                    web:h-[582px] web:w-1/2"
       >
-        <div className="overflow-y-scroll web:pr-[40px] we:w-[100%] web:mr-[24px]">
-          <header className="flex flex-row justify-between">
-            <h2 className="text-[24px] font-medium">Tour locations</h2>
+        <div className="overflow-y-scroll web:pr-[40px] web:mr-[80px]">
+          <header className="flex flex-row justify-between mb-9 tablet:mb-6">
+            <div>
+              <h2 className="text-[24px] tablet:text-[31px] font-medium mb-2 tablet:mb-[22px]">
+                Tour locations
+              </h2>
+              <p className="font-medium text-[18px] tablet:text-[24px]">
+                {title}
+              </p>
+            </div>
             <Link
               className="mt-[7px] tablet:mt-0 text-[#E8B600]"
               href={"/edit"}
@@ -60,27 +78,45 @@ export default function Preview() {
           >
             <GoogleMaps />
           </section>
-          <section className="flex flex-wrap gap-6 mt-[36px] tablet:mt-[24px] web:mt-[36px]">
-            {previewData.length > 0 && (
-              <ul className="text-[16px] text-[#13294B]">
-                {previewData.map((loc, i) => (
+          <section className="flex flex-wrap w-[100%] gap-6 mt-[36px] tablet:mt-[24px] web:mt-[36px]">
+            {locations.length > 0 && (
+              <ul className="text-[16px] text-[#13294B] ">
+                {locations.map((loc, i) => (
                   <li
                     key={i}
                     className="relative ml-5 before:absolute before:block before:content-[''] 
                                 before:bg-[#617086] before:w-[0.5px] before:top-0 before:bottom-0
-                                first:before:top-[15px]"
+                                before:top-[15px] pb-[64px] last:pb-[0px]"
                   >
                     <div className="absolute -left-[7px] w-[15px] h-[15px] border-[0.5px] rounded-full border-[#617086]"></div>
                     <div className="ml-6">
-                      <span className="font-light">Location {i + 1}</span>
-                      <h3 className="font-medium text-[18px] text-[#081120]">
+                      <span className="font-light mb-2">Location {i + 1}</span>
+                      <h3 className="font-medium text-[18px] text-[#081120] mb-4">
                         {loc.locationName}
                       </h3>
-                      <div className="font-semibold text-[#4285F4] flex gap-x-11">
-                        <span>Play Audio</span>
-                        <span>Show directions</span>
+                      <div className="font-semibold text-[#4285F4] flex gap-x-8 tablet:gap-x-11 mb-6 ml-2 tablet:ml-6">
+                        <div className="flex items-center gap-3">
+                          <Image
+                            src={Play}
+                            width={16}
+                            height={18}
+                            alt="Play audio"
+                          />
+                          <span>Play Audio</span>
+                        </div>
+                        <div className="flex items-center gap-3">
+                          <Image
+                            src={ArrowRedo}
+                            width={24}
+                            height={24}
+                            alt="Get location directions"
+                          />
+                          <span>Show directions</span>
+                        </div>
                       </div>
-                      <p className="text-[14px]">{loc.description}</p>
+                      <p className="text-[14px] phone:max-w-[300px] tablet:max-w-[400px]">
+                        {loc.description}
+                      </p>
                     </div>
                   </li>
                 ))}
@@ -93,17 +129,17 @@ export default function Preview() {
         <div className="web:w-1/2">
           <div
             className="web:w-[100%] mt-[64px] tablet:mt-[19px] tablet:mb-[38px] web:mt-[36px] web:mb-[59px] 
-            flex flex-col tablet:flex-row gap-6 tablet:gap-5 web:gap-6 tablet:justify-center"
+            flex flex-col tablet:flex-row-reverse gap-6 tablet:gap-5 web:gap-6 tablet:justify-center"
           >
-            <Btn
-              className="w-full tablet:w-[182px] web:w-[220px] webl:w-[278px] h-[44px] font-semibold"
-              variant="outlined"
-              text="About tour"
-            />
             <Btn
               className="w-full tablet:w-[182px] web:w-[220px] webl:w-[278px] h-[44px] font-semibold"
               variant="filled"
               text="Buy now"
+            />
+            <Btn
+              className="w-full tablet:w-[182px] web:w-[220px] webl:w-[278px] h-[44px] font-semibold"
+              variant="outlined"
+              text="About tour"
             />
           </div>
         </div>
