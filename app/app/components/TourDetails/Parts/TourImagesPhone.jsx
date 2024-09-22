2da7349522a919c6a6e011efb3fe681b83f15eed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-23 00:11:43 +0300

    Update TourImagesPhone.jsx

--- app/app/components/TourDetails/Parts/TourImagesPhone.jsx
+++ app/app/components/TourDetails/Parts/TourImagesPhone.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function TourImagesPhone({ thumbnailImageUrl, landmarks }) {
+  console.log(landmarks);
+  
   return (
     <div
       className="hidden web:hidden tablet:hidden 
@@ -24,17 +26,44 @@ function TourImagesPhone({ thumbnailImageUrl, landmarks }) {
       {landmarks &&
         landmarks.flatMap((landmark, index) =>
           landmark.resources
-            ? landmark.resources.map((resource, resourceIndex) => (
-                <img
-                  key={resource.resourceId}
-                  className="object-cover rounded-[5px]
-                      phone:w-[361px] phone:h-[260px]
-                      smallPhone:w-[361px] smallPhone:h-[260px]
-                      "
-                  src={resource.resourceUrl}
-                  alt={`Landmark Image ${index + 1}`}
-                />
-              ))
+            ? landmark.resources.map((resource, resourceIndex) => {
+                if (resource.resourceType === "Image") {
+                  return (
+                    <img
+                      key={resource.resourceId}
+                      className="object-cover rounded-[5px]
+                          phone:w-[361px] phone:h-[260px]
+                          smallPhone:w-[361px] smallPhone:h-[260px]
+                          "
+                      src={resource.resourceUrl}
+                      alt={`Landmark Image ${index + 1}`}
+                    />
+                  );
+                } else if (resource.resourceType === "Audio") {
+                  return (
+                    <audio
+                      key={resource.resourceId}
+                      controls
+                      className="phone:w-[361px] smallPhone:w-[361px]"
+                    >
+                      <source src={resource.resourceUrl} type="audio/mpeg" />
+                      Your browser does not support the audio element.
+                    </audio>
+                  );
+                } else if (resource.resourceType === "Video") {
+                  return (
+                    <video
+                      key={resource.resourceId}
+                      controls
+                      className="phone:w-[361px] phone:h-[260px] smallPhone:w-[361px] smallPhone:h-[260px]"
+                    >
+                      <source src={resource.resourceUrl} type="video/mp4" />
+                      Your browser does not support the video element.
+                    </video>
+                  );
+                }
+                return null; 
+              })
             : []
         )}
     </div>
