3d1ced074326996f7d0cc474fd4f5b1b158b26e1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-25 18:37:39 +0300

    Update BgBiggestTowns.jsx

--- app/app/components/HomePage/BgBiggestTowns/BgBiggestTowns.jsx
+++ app/app/components/HomePage/BgBiggestTowns/BgBiggestTowns.jsx
@@ -33,59 +33,72 @@ function BgBiggestTowns() {
   return (
     <div
       className="flex flex-col items-center justify-center gap-[30px] w-full h-full
-        web:max-w-[1792px] web:min-h-[752px] web:gap-[30px]
-        tablet:px-[10px] tablet:gap-[30px] tablet:min-h-[450px]
-        phone:gap-[20px] phone:mb-[50px]
-        smallPhone:gap-[20px] smallPhone:mb-[50px]"
+      web:max-w-[1792px] web:min-h-[752px] web:gap-[30px]
+      tablet:px-[10px] tablet:gap-[30px] tablet:min-h-[450px]
+      phone:gap-[20px] phone:mb-[50px]
+      smallPhone:gap-[20px] smallPhone:mb-[50px]
+      "
     >
       <div
-        className="flex items-center max-w-[1792px] w-[95%]
-        web:justify-between web:mb-[50px]
-        tablet:justify-between tablet:mb-[30px]
-        phone:justify-center"
+        className="flex items-center w-[95%] max-w-[1792px]
+      web:justify-between web:mb-[50px]
+      tablet:justify-between tablet:mb-[30px]
+      phone:justify-center
+      "
       >
         <h2
           className="text-[#081120] font-medium font-['Inter'] leading-[58.50px]
-            web:text-[39px]
-            tablet:text-[31px]
-            phone:text-base 
-            smallPhone:text-base smallPhone:px-[10px]"
+          web:text-[39px]
+          tablet:text-[31px]
+          phone:text-base 
+          smallPhone:text-base smallPhone:px-[10px]
+          "
         >
           Explore Biggest Towns in Bulgaria
         </h2>
         <Link
           className="hidden space-x-2 items-start
-          web:flex web:h-[19px]
-          tablet:flex tablet:h-[19px]"
+        web:flex web:h-[19px]
+        tablet:flex tablet:h-[19px]
+        phone:
+        smallPhone:
+      "
           href="/explore"
         >
           <span
             className="text-[#081120] text-base font-semibold font-['Inter']
-            web:w-[74px] web:h-[19px] web:text-base"
+          web:w-[74px] web:h-[19px] web:text-base
+          tablet:w-[74px] tablet:h-[19px] tablet:text-base
+          phone:
+          smallPhone:
+          "
           >
             See More
           </span>
           <SeeMoreSvgHomePage
             className="hidden
-            web:w-6 web:h-6 web:block
-            tablet:w-6 tablet:h-6 tablet:block"
+          web:w-6 web:h-6 web:block
+          tablet:w-6 tablet:h-6 tablet:block
+          phone:
+          smallPhone:
+          "
           />
         </Link>
       </div>
-
       <div
-        className="flex items-center justify-center w-full px-[20px] hideScroll
-        overflow-x-auto
-        web:max-w-[1792px] web:gap-[5%]
-        tablet:gap-[15px] tablet:justify-center
-        phone:gap-[0px] smallPhone:gap-[0px]"
+        className="flex items-center justify-evenly w-full 
+    web:max-w-[1792px] web:gap-[25px]
+    smallPhone:gap-[10px]
+    "
       >
         {exploreBiggestCities.map((place, index) => (
-          <div key={index} className="inline-block">
+          <div key={index} >
             <CardSphera
               thumbnailImageUrl={place.thumbnailImageUrl}
               destination={place.destination}
-              onClick={() => router.push(`/explore?search=${place.destination}`)}
+              onClick={() =>
+                router.push(`/explore?search=${place.destination}`)
+              }
             />
           </div>
         ))}
@@ -95,4 +108,3 @@ function BgBiggestTowns() {
 }
 
 export default BgBiggestTowns;
-
