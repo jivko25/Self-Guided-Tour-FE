5b15c14ebfbecde674c2375e3943810c4d957cb9
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-09-29 13:36:27 +0300

    chore: added hover effect ot Btn component

--- app/app/components/Buttons/Btn.jsx
+++ app/app/components/Buttons/Btn.jsx
@@ -15,11 +15,11 @@ export default function Btn({
   ...props
 }) {
   const variantClasses = {
-    filled: "bg-blue-950 text-white border-2 rounded-md border-transparent",
-    outlined: "bg-neutral text-gray-900 border-2 rounded-md border-blue-950",
-    transparent: "bg-transparent text-gray-900 border-transparent",
+    filled: "bg-blue-950 text-white border-2 rounded-md border-transparent hover:bg-opacity-70",
+    outlined: "bg-neutral text-gray-900 border-2 rounded-md border-blue-950 hover:text-opacity-70 hover:border-[#617086]",
+    transparent: "bg-transparent text-gray-900 border-transparent hover:text-opacity-70",
     "transparent-outlined":
-      "bg-transparent text-gray-900 rounded-md border-2 border-blue-950",
+      "bg-transparent text-gray-900 rounded-md border-2 border-blue-950 hover:text-opacity-70 hover:border-[#617086]",
   };
 
   const commonClasses = `h-11 px-4 py-3 justify-center items-center inline-flex text-center font-medium 
