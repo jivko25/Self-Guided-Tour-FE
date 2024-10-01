09b9104dafbb4b6ea345b30867cc5629e2e4148a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-01 20:14:27 +0300

    Update TourInfo.jsx

--- app/app/components/TourDetails/Parts/TourInfo.jsx
+++ app/app/components/TourDetails/Parts/TourInfo.jsx
@@ -5,10 +5,10 @@ import LocationSharp from "../Svgs/LocationSharp";
 function TourInfo({ estimatedDuration, destination }) {
   return (
     <div
-        className="flex border-b-2  border-[#d1d0d8] 
+        className="flex border-b-2 border-[#d1d0d8] 
         web:h-[250px] web:w-[80%] web:justify-around web:items-center
         tablet:flex-row-reverse tablet:h-[250px] tablet:w-[95%] tablet:justify-center tablet:gap-[20px] tablet:items-center
-        flex-col-reverse items-start justify-center gap-[30px] w-[95%] py-[40px]"
+        flex-col-reverse items-start justify-center gap-[30px] w-[100%] py-[40px]"
       >
         <div className="flex items-center">
           <Walk className="
