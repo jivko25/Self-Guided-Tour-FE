9a96050b09c82fff6564b7c70d9bb5a8b8bd72b6
Михаел Йорданов <gaws@mail.bg>
2024-10-03 10:30:40 +0300

    implement drop down menu

---
+++ app/app/components/DropDownMenu/DropDownMenu.jsx
@@ -0,0 +1,53 @@
+import IconButton from "../Buttons/IconButton";
+import AboutUsIcon from "@/app/public/svg/compass.svg";
+import HelpIcon from "@/app/public/help-circle-outline.svg";
+import AccountIcon from "@/app/public/svg/person.svg";
+import Btn from "../Buttons/Btn";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+function DropDownMenu({ onSignOut }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useRouter();
+  function handleClick(path) {
+    setIsMenuOpen((prev) => !prev);
+    navigate.push(path);
+  }
+  return (
+    <div className="z-20 relative">
+      <div className="w-32">
+        <Btn
+          text="Menu"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          variant="filled-white"
+          fullWidth={true}
+        />
+      </div>
+      {isMenuOpen && (
+        <div
+          className="h-[215px] w-[170px] bg-[#FAFAFA] rounded-[5px] 
+        flex flex-col  items-center gap-8 z-10 absolute
+         left-1/2 -translate-x-1/2 mt-3 "
+        >
+          <div className="flex flex-col items-start gap-3 mr-6 mt-6 ">
+            <IconButton
+              text="Account"
+              icon={AccountIcon}
+              className="w-20"
+              onClick={() => handleClick("/profile/my-profile")}
+            />
+            <IconButton text="About Us" icon={AboutUsIcon} className="w-20" />
+            <IconButton text="Help" icon={HelpIcon} className="w-20" />
+          </div>
+          <Btn
+            variant="transparent-outlined"
+            text="Sign Out"
+            onClick={onSignOut}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DropDownMenu;
