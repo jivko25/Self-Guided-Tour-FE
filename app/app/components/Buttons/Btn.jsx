8a6ea608765d187ee8750ce64993f466f40aa770
Михаел Йорданов <gaws@mail.bg>
2024-10-03 10:29:54 +0300

    Add Filled-white variant

--- app/app/components/Buttons/Btn.jsx
+++ app/app/components/Buttons/Btn.jsx
@@ -15,11 +15,16 @@ export default function Btn({
   ...props
 }) {
   const variantClasses = {
-    filled: "bg-blue-950 text-white border-2 rounded-md border-transparent hover:bg-opacity-70",
-    outlined: "bg-neutral text-gray-900 border-2 rounded-md border-blue-950 hover:text-opacity-70 hover:border-[#617086]",
-    transparent: "bg-transparent text-gray-900 border-transparent hover:text-opacity-70",
+    filled:
+      "bg-blue-950 text-white border-2 rounded-md border-transparent hover:bg-opacity-70",
+    outlined:
+      "bg-neutral text-gray-900 border-2 rounded-md border-blue-950 hover:text-opacity-70 hover:border-[#617086]",
+    transparent:
+      "bg-transparent text-gray-900 border-transparent hover:text-opacity-70",
     "transparent-outlined":
       "bg-transparent text-gray-900 rounded-md border-2 border-blue-950 hover:text-opacity-70 hover:border-[#617086]",
+    "filled-white":
+      "bg-[#FAFAFA] text-blue-950 border-[#FAFAFA] rounded-md  hover:bg-opacity-70 hover:text-opacity-70",
   };
 
   const commonClasses = `h-11 px-4 py-3 justify-center items-center inline-flex text-center font-medium 
