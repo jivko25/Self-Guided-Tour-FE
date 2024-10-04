235c2be1a25d7a729069e173aa9581e1d4a08bf6
Михаел Йорданов <gaws@mail.bg>
2024-10-04 16:45:20 +0300

    move to home page on logout

--- app/app/components/Header/Header.jsx
+++ app/app/components/Header/Header.jsx
@@ -5,28 +5,33 @@ import HeaderMobile from "./HeaderMobile.jsx";
 import { useEffect, useState } from "react";
 import { useAuth } from "@/app/context/authContext";
 import { usePathname, useRouter } from "next/navigation";
-import { getUserSession, logoutUser, validateToken } from "@/app/actions/authActions";
+import {
+  getUserSession,
+  logoutUser,
+  validateToken,
+} from "@/app/actions/authActions";
+import { set } from "react-hook-form";
 
 export default function Header() {
   const { session, setSession } = useAuth();
   const [headerVisible, setHeaderVisible] = useState(false);
   const pathname = usePathname();
   const router = useRouter();
-  
+
   useEffect(() => {
-    validateToken().then(data => {
+    validateToken().then((data) => {
       if (data?.error) {
         console.log(data.error);
-        router.push('/');
+        router.push("/");
       }
     });
 
-    getUserSession().then(result => {
+    getUserSession().then((result) => {
       setSession(result);
     });
-    
+
     // handle mobile menu visibility
-    if (pathname === '/sign-in' || pathname === '/create-account') {
+    if (pathname === "/sign-in" || pathname === "/create-account") {
       setHeaderVisible(true);
     } else {
       setHeaderVisible(false);
@@ -41,16 +46,17 @@ export default function Header() {
     } else {
       setSession(false);
     }
-  }
+    router.push("/");
+  };
 
   return (
     <>
       <header className="hidden web:flex w-full  items-center justify-center h-100px py-9">
-        <Web isAuthenticated={session} handleLogout={handleLogout}/>
+        <Web isAuthenticated={session} handleLogout={handleLogout} />
       </header>
 
       <header className="hidden tablet:flex web:hidden items-center justify-center w-full h-100px">
-        <Tablet isAuthenticated={session} />
+        <Tablet isAuthenticated={session} handleLogout={handleLogout} />
       </header>
 
       <header className="tablet:hidden z-50" hidden={headerVisible}>
