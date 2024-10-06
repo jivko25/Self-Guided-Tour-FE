10c7b67a592171436130df02fba3539fe1c31101
Михаел Йорданов <gaws@mail.bg>
2024-10-06 23:11:01 +0300

    Remove Lorem

--- app/app/components/Table/TourDetails/TourSummary.jsx
+++ app/app/components/Table/TourDetails/TourSummary.jsx
@@ -2,12 +2,7 @@ function TourSummary({ tour }) {
   return (
     <section className="font-medium max-w-96 text-left hidden web:block">
       <h1>Summary</h1>
-      <p className="font-normal text-wrap">
-        {tour.summary} Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Rem cum tempora in sit optio quam ut a inventore architecto. Deleniti
-        temporibus esse magni quaerat, dolor fuga distinctio incidunt pariatur
-        facilis!
-      </p>
+      <p className="font-normal text-wrap">{tour.summary}</p>
     </section>
   );
 }
