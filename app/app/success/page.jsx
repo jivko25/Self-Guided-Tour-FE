d80f8432edfed500834c8ad92551c9f6193939c9
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2024-10-07 20:05:52 +0300

    chore: changed styles in success page

--- app/app/success/page.jsx
+++ app/app/success/page.jsx
@@ -4,26 +4,25 @@ import Btn from "../components/Buttons/Btn.jsx";
 
 export default function TourSuccess() {
   return (
-    <main className="flex flex-col items-center align-middle justify-center w-full  my-auto tablet:my-20 web:my-0  ">
-      <h1 className="text-[#081120] w-[300px]    text-center text-xl font-medium font-['Inter'] leading-[46.50px]  tablet:mb-2 tablet:w-[540px]  tablet:text-[31px] web:w-full">
+    <main className="flex flex-col items-center align-middle justify-center w-full my-auto tablet:my-32 web:my-0 px-[16px] tablet:mb-64 web:mb-48">
+      <h1 className="text-[#081120] max-w-[361px] tablet:max-w-[584px] text-center text-xl font-medium mb-6 tablet:text-[31px] tablet:leading-[46.5px] web:max-w-full">
         Congrats! We received your tour submission
       </h1>
-      <p className=" text-center w-[300px]  text-[#13294b] text-base font-normal font-['Inter'] leading-[30px] tablet:w-[500px]  tablet:text-xl">
+      <p className=" text-center max-w-[361px] tablet:max-w-[584px] text-[#13294b] text-base font-normal leading-[30px] tablet:text-xl">
         After a quick review we will notify for your tour status. You can check
         it in My tours section
       </p>
       <Image
-        className="mt-6 w-[393px]  tablet:w-fit "
+        className="mt-6 w-[393px] tablet:w-fit"
         src={TourSuccessImage}
         alt="TourSubmissionSuccess"
-        height={0}
-        width={0}
+        style={{height: 'auto'}}
       />
-      <section className="flex w-full h-40 flex-col gap-4  tablet:h-fit tablet:flex-row items-center justify-center">
-        <div className="w-[90%] tablet:w-[183px] tablet:order-2 web:w-[278px]">
+      <section className="flex w-full h-40 flex-col gap-4 tablet:h-fit tablet:flex-row items-center justify-center">
+        <div className="w-full tablet:w-[181px] tablet:order-2 web:w-[279px]">
           <Btn fullWidth link="/my-tours" variant="filled" text="My Tours" />
         </div>
-        <div className="w-[90%] tablet:w-[183px] web:w-[278px]">
+        <div className="w-full tablet:w-[181px] web:w-[279px]">
           <Btn fullWidth link="/" variant="outlined" text="Home" />
         </div>
       </section>
