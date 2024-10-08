58b8910aed74529e9691edbbf1c6fb9cb9dee3e2
Михаел Йорданов <gaws@mail.bg>
2024-10-08 07:22:19 +0300

    wire up delete

--- app/app/components/Table/TableComponents/MyLibraryTableRow.jsx
+++ app/app/components/Table/TableComponents/MyLibraryTableRow.jsx
@@ -8,15 +8,22 @@ import TourDetails from "../TourDetails/TourDetails";
 import TourSummary from "../TourDetails/TourSummary";
 import TourStatus from "../../TourStatus/TourStatus";
 import EyeIcon from "@/app/public/svg/view.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Btn from "../../Buttons/Btn";
 import ImageSkeleton from "../../Skeletons/ImageSkeleton";
 import StatusDot from "./StatusDot";
 import { useRouter } from "next/navigation";
-function TableRow({ tour, activeTab }) {
+import { useProfile } from "@/app/context/profileContext";
+import { AnimatePresence } from "framer-motion";
+import Modal from "@/app/components/Notification/Modal";
+import { usePopup } from "@/app/context/popupContext";
+function TableRow({ tour, activeTab, triggerRerender }) {
   const [showDetails, setShowDetails] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const { deleteTourAsync } = useProfile();
   const navigate = useRouter();
+  const popup = usePopup();
   function handleEdit(tourId) {
     navigate.push(`/create?edit=${tourId}`);
   }
@@ -26,7 +33,18 @@ function TableRow({ tour, activeTab }) {
   function handleShowDetails() {
     setShowDetails(!showDetails);
   }
-
+  async function onDelete(tourId) {
+    setShowModal(false);
+    await deleteTourAsync(tourId);
+    popup({ type: "SUCCESS", message: "Tour deleted successfully" });
+    triggerRerender();
+  }
+  // Scroll to top when modal is opened
+  useEffect(() => {
+    if (showModal) {
+      window.scrollTo(0, 0);
+    }
+  }, [showModal]);
   return (
     <tr key={tour.id} className="border-b-2 text-center ">
       <td className=" text-center max-w-32">
@@ -94,6 +112,7 @@ function TableRow({ tour, activeTab }) {
                 {tour.status !== "Under Review" && (
                   <IconButton
                     icon={TrashIcon}
+                    onClick={() => setShowModal(true)}
                     className="border-[3px] p-2 border-[#E80000] rounded-[5px]  "
                   />
                 )}
@@ -120,6 +139,18 @@ function TableRow({ tour, activeTab }) {
           )}
         </div>
       </td>
+      <AnimatePresence>
+        {showModal && (
+          <Modal
+            title={"Are you sure you want to delete this tour?"}
+            description={
+              "This action cannot be undone. This will permanently delete the tour."
+            }
+            onClose={() => setShowModal(false)}
+            onConfirm={() => onDelete(tour.tourId)}
+          ></Modal>
+        )}
+      </AnimatePresence>
     </tr>
   );
 }
